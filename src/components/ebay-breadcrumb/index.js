832be4a574d0ee731a21c91813945cf9e989f3e8
Raja Ramu <raja.skk@gmail.com>
2018-04-03 09:41:05 -0700

    New Component: Breadcrumbs (#66)

---
+++ src/components/ebay-breadcrumb/index.js
@@ -0,0 +1,43 @@
+const markoWidgets = require('marko-widgets');
+const emitAndFire = require('../../common/emit-and-fire');
+const processHtmlAttributes = require('../../common/html-attributes');
+const template = require('./template.marko');
+
+function getTemplateData(state, input) {
+    const htmlAttributes = processHtmlAttributes(input);
+    const inputItems = input.items || [];
+    let items = (inputItems).map((item, index) => {
+        const itemHtmlAttributes = processHtmlAttributes(item);
+        let tag = 'a';
+        let ariaCurrent = null;
+        const href = item.href || null;
+        const current = ((inputItems.length - 1) === index);
+        if (current && !href) {
+            tag = 'span';
+            ariaCurrent = 'page';
+        }
+        return {
+            tag,
+            htmlAttributes: itemHtmlAttributes,
+            renderBody: item.renderBody,
+            href,
+            ariaCurrent
+        };
+    });
+    items = Object.keys(items).length > 0 ? items : null;
+    return {
+        items,
+        headingText: input.headingText || '',
+        headingLevel: input.headingLevel || 'h2',
+        htmlAttributes
+    };
+}
+function handleClick(event) {
+    emitAndFire(this, 'breadcrumb-select', { el: event.target });
+}
+
+module.exports = markoWidgets.defineComponent({
+    template,
+    getTemplateData,
+    handleClick
+});
