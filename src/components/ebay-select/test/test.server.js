5ab59bd289661e3115eb30b2d409601131c816f2
Yoni Medoff <yonimedoff@gmail.com>
2018-04-25 18:39:19 -0700

    Select: fix pass-through on nested options

--- src/components/ebay-select/test/test.server.js
+++ src/components/ebay-select/test/test.server.js
@@ -9,33 +9,53 @@ const options = [{
 }];
 const emptyOptions = [];
 
-test('renders basic version', context => {
-    const input = { options };
-    const $ = testUtils.getCheerio(context.render(input));
-    expect($('.combobox').length).to.equal(1);
-    expect($('.combobox__control').length).to.equal(1);
-    expect($('.combobox__options[role=listbox]').length).to.equal(1);
-    expect($('.combobox__option[role=option]').length).to.equal(2);
-    expect($('.combobox__option[role=option][aria-selected="true"]').length).to.equal(1);
+describe('select', () => {
+    test('renders basic version', context => {
+        const input = { options };
+        const $ = testUtils.getCheerio(context.render(input));
+        expect($('.combobox').length).to.equal(1);
+        expect($('.combobox__control').length).to.equal(1);
+        expect($('.combobox__options[role=listbox]').length).to.equal(1);
+        expect($('.combobox__option[role=option]').length).to.equal(2);
+        expect($('.combobox__option[role=option][aria-selected="true"]').length).to.equal(1);
+    });
+
+    test('renders empty', context => {
+        const input = { emptyOptions };
+        const $ = testUtils.getCheerio(context.render(input));
+        expect($('.combobox').length).to.equal(1);
+        expect($('.combobox__control').length).to.equal(1);
+        expect($('.combobox__options[role=listbox]').length).to.equal(0);
+        expect($('.combobox__option[role=option]').length).to.equal(0);
+    });
+
+    test('renders with second item selected', context => {
+        const input = { options };
+        input.options[0].selected = false;
+        input.options[1].selected = true;
+        const $ = testUtils.getCheerio(context.render(input));
+        expect($('.combobox').length).to.equal(1);
+        expect($('.combobox__control').length).to.equal(1);
+        expect($('.combobox__options[role=listbox]').length).to.equal(1);
+        expect($('.combobox__option[role=option]').length).to.equal(2);
+        expect($('.combobox__option[role=option][aria-selected="true"]:nth-child(2)').length).to.equal(1);
+    });
+
+    test('handles pass-through html attributes', context => {
+        testUtils.testHtmlAttributes(context, 'span.combobox');
+    });
+
+    test('handles custom class', context => {
+        testUtils.testCustomClass(context, 'span.combobox');
+    });
 });
 
-test('renders empty', context => {
-    const input = { emptyOptions };
-    const $ = testUtils.getCheerio(context.render(input));
-    expect($('.combobox').length).to.equal(1);
-    expect($('.combobox__control').length).to.equal(1);
-    expect($('.combobox__options[role=listbox]').length).to.equal(0);
-    expect($('.combobox__option[role=option]').length).to.equal(0);
-});
+describe('select-option', () => {
+    test('handles pass-through html attributes', context => {
+        testUtils.testHtmlAttributes(context, '.combobox__option', 'options');
+    });
 
-test('renders with second item selected', context => {
-    const input = { options };
-    input.options[0].selected = false;
-    input.options[1].selected = true;
-    const $ = testUtils.getCheerio(context.render(input));
-    expect($('.combobox').length).to.equal(1);
-    expect($('.combobox__control').length).to.equal(1);
-    expect($('.combobox__options[role=listbox]').length).to.equal(1);
-    expect($('.combobox__option[role=option]').length).to.equal(2);
-    expect($('.combobox__option[role=option][aria-selected="true"]:nth-child(2)').length).to.equal(1);
+    test('handles custom class', context => {
+        testUtils.testCustomClass(context, '.combobox__option', 'options');
+    });
 });
