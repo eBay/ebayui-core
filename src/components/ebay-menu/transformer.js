dbfc1038a86d7a3bcd288ceab0a5ae9c51c8cf5e
Yoni Medoff <yonimedoff@gmail.com>
2018-07-11 18:28:48 -0700

    start menu icon with transformer

---
+++ src/components/ebay-menu/transformer.js
@@ -0,0 +1,25 @@
+const commonTransformer = require('../../common/transformer');
+
+function transform(el, context) {
+    commonTransformer(el, context);
+    const builder = context.builder;
+    const iconAttribute = el.getAttribute('icon');
+    const iconName = iconAttribute && iconAttribute.value.value;
+    if (iconName) {
+        const iconTag = context.createNodeForEl('ebay-icon', [
+            {
+                name: 'name',
+                value: builder.literal(iconName)
+            },
+            {
+                name: 'type',
+                value: builder.literal('inline')
+            }
+        ]);
+        el.replaceWith(iconTag);
+        // el.setAttributeValue('foo', context.builder.literal('bar'));
+        // el.prependChild(builder.htmlComment(builder.literal('foobar')));
+    }
+}
+
+module.exports = transform;
