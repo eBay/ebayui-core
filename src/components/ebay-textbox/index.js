b228eb914b23811af2568aa40dcb128e41ff1d8a
anudeepdasari <adasari@ebay.com>
2018-07-26 14:54:52 -0700

    Added invalid attribute to template data

--- src/components/ebay-textbox/index.js
+++ src/components/ebay-textbox/index.js
@@ -3,21 +3,20 @@ const processHtmlAttributes = require('../../common/html-attributes');
 const template = require('./template.marko');
 
 function getInitialState(input) {
-    const htmlAttributes = input.invalid ?
-        Object.assign({}, processHtmlAttributes(input), { 'aria-invalid': 'true' }) : processHtmlAttributes(input);
     const classes = ['textbox__control'];
     if (input.fluid) {
         classes.push('textbox__control--fluid');
     }
     return {
-        htmlAttributes,
         classes,
-        rootClass: input.class,
+        htmlAttributes: processHtmlAttributes(input),
+        rootClass: input.class || '',
         value: input.value,
         tag: input.fluid ? 'div' : 'span',
         textBoxTag: Boolean(input.multiline) ? 'textarea' : 'input',
         disabled: Boolean(input.disabled),
-        placeholder: input.placeholder
+        placeholder: input.placeholder,
+        invalid: String(Boolean(input.invalid))
     };
 }
 
