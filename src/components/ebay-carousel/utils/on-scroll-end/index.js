3346c2de05533311221f8acbe39e89fadcdde982
Dylan Piercey <pierceydylan@gmail.com>
2018-10-25 12:21:51 -0700

    Carousel: Improve native scrolling animation on iOS (#458)

--- src/components/ebay-carousel/utils/on-scroll-end/index.js
+++ src/components/ebay-carousel/utils/on-scroll-end/index.js
@@ -11,31 +11,47 @@
 module.exports = function onScrollEnd(el, fn) {
     let frame;
     let timeout;
+    let lastTime;
+    let lastLeft;
     let stage = 0;
     el.addEventListener('touchmove', handleTouchMove);
 
     return cancel;
 
     // First we wait for a touch move (means scrolling as started).
-    function handleTouchMove() {
-        stage++;
-        cancelTouchMove();
-        el.addEventListener('touchend', handleTouchEnd);
+    function handleTouchMove(e) {
+        if (stage !== 1) {
+            el.addEventListener('touchend', handleTouchEnd);
+        }
+
+        stage = 1;
+        lastTime = e.timeStamp;
+        lastLeft = el.scrollLeft;
     }
 
     // Then we wait for the touch to end (user has stopped scrolling).
-    function handleTouchEnd() {
-        stage++;
+    function handleTouchEnd(e) {
+        stage = 2;
         cancelTouchEnd();
+
+        if (e.cancelable) {
+            const time = e.timeStamp - lastTime;
+            const distance = el.scrollLeft - lastLeft;
+            const velocity = distance / time;
+            stage = 0;
+            // If it's cancelable then this browser does not inertial scroll by default and we can animate immediately.
+            return fn(el.scrollLeft, velocity);
+        }
+
         el.addEventListener('touchstart', handleTouchStart);
         frame = requestAnimationFrame(() => checkScrollEnded(el.scrollLeft));
     }
 
     // If the user touches again before the inertial scrolling has stopped then we reset.
-    function handleTouchStart() {
-        cancel();
-        stage--;
-        el.addEventListener('touchend', handleTouchEnd);
+    function handleTouchStart(e) {
+        stage = 0;
+        cancelPolling();
+        handleTouchMove(e);
     }
 
     // Finally after the touch end we poll the scroll state every animation frame
@@ -48,9 +64,8 @@ module.exports = function onScrollEnd(el, fn) {
                     checkScrollEnded(newOffset);
                 } else {
                     cancelTouchStart();
-                    fn(newOffset);
+                    fn(newOffset, 0);
                     stage = 0;
-                    el.addEventListener('touchmove', handleTouchMove);
                 }
             });
         }, 64);
@@ -68,14 +83,24 @@ module.exports = function onScrollEnd(el, fn) {
         el.removeEventListener('touchstart', handleTouchStart);
     }
 
-    function cancel() {
+    function cancelPolling() {
         cancelAnimationFrame(frame);
         clearTimeout(timeout);
+    }
+
+    function cancel() {
+        cancelTouchMove();
 
         switch (stage) {
-            case 0: cancelTouchMove(); break;
-            case 1: cancelTouchEnd(); break;
-            default: cancelTouchStart(); break;
+            case 1:
+                cancelTouchEnd();
+                break;
+            case 2:
+                cancelPolling();
+                cancelTouchStart();
+                break;
+            default:
+                break;
         }
     }
 };
