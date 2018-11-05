b492517a8a8a1ebe864ba0439bf938ad7e22b75f
Sean Gates <sgates@ebay.com>
2018-11-05 11:04:28 -0700

    Adding better accessibility

--- src/common/multiline-ellipsis/test/test.browser.js
+++ src/common/multiline-ellipsis/test/test.browser.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect;
 const multilineEllipsis = require('../');
 
-describe('element-scroll', () => {
+describe('multiline-ellipsis', () => {
     const contentDiv = document.createElement('div');
     contentDiv.setAttribute('style', 'height: 50px; width: 180px;');
     contentDiv.innerHTML = 'Pariatur est ut nisi sint voluptate in nostrud amet dolore ullamco ex anim';
@@ -17,7 +17,7 @@ describe('element-scroll', () => {
         document.body.removeChild(contentDiv);
     });
 
-    test('adds two spans to separate content strings for accessibility', () => {
+    test('adds two spans for accessibility', () => {
         expect(spans.length).to.equal(2);
         expect(spans[0].getAttribute('aria-hidden')).to.equal('true');
         expect(spans[1].className.indexOf('clipped')).to.not.equal(-1);
--- src/components/ebay-pill/index.js
+++ src/components/ebay-pill/index.js
@@ -1,31 +1,56 @@
 const markoWidgets = require('marko-widgets');
-const processHtmlAttributes = require('../../common/html-attributes');
 const multilineEllipsis = require('../../common/multiline-ellipsis');
 const template = require('./template.marko');
-const pillSelector = 'button.btn--pill';
+const pillSelector = 'button.btn--pill, a.fake-btn--pill';
 
 function getInitialState(input) {
     return {
-        htmlAttributes: processHtmlAttributes(input),
         checked: input.checked,
         disabled: input.disabled,
         style: input.style,
-        href: input.href
+        class: input.class,
+        href: input.href,
+        ariaSelectedText: input.ariaSelectedText || 'Selected'
     };
 }
 
 function getTemplateData(state) {
+    let buttonPillClass;
+
+    if (state.href) {
+        buttonPillClass = 'fake-btn--pill';
+    } else {
+        buttonPillClass = 'btn--pill';
+    }
+
+    state.classes = [buttonPillClass, state.class];
     state.priority = (state.checked ? 'primary' : 'secondary');
+
     return state;
 }
 
 function onRender() {
     // determine whether the content overflows
     multilineEllipsis.truncate(this.el.querySelector(pillSelector));
+
+    const pillBtn = this.el.querySelector(pillSelector);
+    const selectedTextSpan = pillBtn.querySelector('.pill__selected-text');
+
+    if (this.state.href && this.state.checked && !selectedTextSpan) {
+        const selectedSpan = document.createElement('span');
+        selectedSpan.setAttribute('class', 'pill__selected-text clipped');
+        selectedSpan.innerHTML = ` - ${this.state.ariaSelectedText}`;
+        pillBtn.appendChild(selectedSpan);
+    } else {
+        if (selectedTextSpan) {
+            pillBtn.removeChild(selectedTextSpan);
+        }
+    }
 }
 
 function init() {
-    this.el.querySelector(pillSelector).addEventListener('click', () => {
+    const pillBtn = this.el.querySelector(pillSelector);
+    pillBtn.addEventListener('click', () => {
         const newCheckedState = !this.state.checked;
         this.setState('checked', newCheckedState);
     });
