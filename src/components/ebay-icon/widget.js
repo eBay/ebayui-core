642ec5d52863c9c0b464ccf8fe7ce7b8b5a99ee7
Dylan Piercey <pierceydylan@gmail.com>
2019-04-01 14:27:22 -0700

    Refactor ebay-icon to keep the DOM more inline with what Marko expects (#603)

--- src/components/ebay-icon/widget.js
+++ src/components/ebay-icon/widget.js
@@ -12,13 +12,17 @@ function init() {
 
     // If there were any symbols rendered then we move them to the svg above after rendering them.
     const defs = this.getEl('defs');
-    const symbol = defs && defs.querySelector('symbol');
 
-    if (symbol) {
-        // Here we get the name of the symbol by removing the `icon-` part.
-        // We then mark this symbol as `defined` so that no other `ebay-icons` render it.
-        defined[symbol.id.slice(5)] = true;
-        rootSvg.appendChild(symbol);
+    if (defs) {
+        const symbol = defs.querySelector('symbol');
+        defs.parentNode.removeChild(defs);
+
+        if (symbol) {
+            // Here we get the name of the symbol by removing the `icon-` part.
+            // We then mark this symbol as `defined` so that no other `ebay-icons` render it.
+            defined[symbol.id.slice(5)] = true;
+            rootSvg.appendChild(symbol);
+        }
     }
 }
 
