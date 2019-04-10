9a5ba88f51257a826282359b02fa45840ba1eeb4
Dylan Piercey <dpiercey@ebay.com>
2019-04-10 12:07:35 -0700

    Additional refactor ebay breadcrumb

--- src/components/ebay-breadcrumb/index.js
+++ src/components/ebay-breadcrumb/index.js
@@ -1,7 +1,6 @@
-const markoWidgets = require('marko-widgets');
 const emitAndFire = require('../../common/emit-and-fire');
 
-module.exports = markoWidgets.defineComponent({
+module.exports = require('marko-widgets').defineComponent({
     template: require('./template.marko'),
     handleClick(originalEvent) {
         emitAndFire(this, 'breadcrumb-select', { originalEvent, el: originalEvent.target });
