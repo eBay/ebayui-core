88cb8db91a32f2a1ec9a8fe5c42ede3b9663bedb
Dylan Piercey <dpiercey@ebay.com>
2019-04-10 12:06:21 -0700

    Refactor checkbox

--- src/components/ebay-checkbox/index.js
+++ src/components/ebay-checkbox/index.js
@@ -1,33 +1,12 @@
-const markoWidgets = require('marko-widgets');
 const emitAndFire = require('../../common/emit-and-fire');
-const processHtmlAttributes = require('../../common/html-attributes');
-const template = require('./template.marko');
 
-function getInitialState(input) {
-    return {
-        htmlAttributes: processHtmlAttributes(input),
-        classes: ['checkbox', input.class],
-        style: input.style,
-        disabled: Boolean(input.disabled)
-    };
-}
-
-function getTemplateData(state) {
-    return state;
-}
-
-function handleClick(originalEvent) {
-    if (!this.state.disabled) {
+module.exports = require('marko-widgets').defineComponent({
+    template: require('./template.marko'),
+    handleChange(originalEvent) {
         emitAndFire(this, 'checkbox-change', {
             originalEvent,
             value: originalEvent.target.value,
-            checked: originalEvent.target.checked });
+            checked: originalEvent.target.checked
+        });
     }
-}
-
-module.exports = markoWidgets.defineComponent({
-    template,
-    getInitialState,
-    getTemplateData,
-    handleClick
 });
