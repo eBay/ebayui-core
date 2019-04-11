22c8f12bf8bd938905b52240f6409631a04c4b5c
Dylan Piercey <dpiercey@ebay.com>
2019-04-11 11:11:10 -0700

    Refactor radio

--- src/components/ebay-radio/index.js
+++ src/components/ebay-radio/index.js
@@ -1,30 +1,15 @@
-const markoWidgets = require('marko-widgets');
 const emitAndFire = require('../../common/emit-and-fire');
-const processHtmlAttributes = require('../../common/html-attributes');
-const template = require('./template.marko');
 
-function getInitialState(input) {
-    return {
-        htmlAttributes: processHtmlAttributes(input),
-        classes: ['radio', input.class],
-        style: input.style,
-        disabled: Boolean(input.disabled)
-    };
-}
+module.exports = require('marko-widgets').defineComponent({
+    template: require('./template.marko'),
+    handleClick(originalEvent) {
+        const target = originalEvent.target;
 
-function getTemplateData(state) {
-    return state;
-}
-
-function handleClick(originalEvent) {
-    if (!this.state.disabled) {
-        emitAndFire(this, 'radio-change', { originalEvent, value: originalEvent.target.value });
+        if (!target.disabled) {
+            emitAndFire(this, 'radio-change', {
+                originalEvent,
+                value: target.value
+            });
+        }
     }
-}
-
-module.exports = markoWidgets.defineComponent({
-    template,
-    getInitialState,
-    getTemplateData,
-    handleClick
 });
