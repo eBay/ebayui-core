f53fa5c25e3560e2acd55883fbe8c3d075165f53
Dylan Piercey <dpiercey@ebay.com>
2019-04-11 12:50:49 -0700

    Refactor switch

--- src/components/ebay-switch/index.js
+++ src/components/ebay-switch/index.js
@@ -1,34 +1,12 @@
-const markoWidgets = require('marko-widgets');
 const emitAndFire = require('../../common/emit-and-fire');
-const processHtmlAttributes = require('../../common/html-attributes');
-const template = require('./template.marko');
 
-function getInitialState(input) {
-    return {
-        htmlAttributes: processHtmlAttributes(input),
-        classes: ['switch', input.class],
-        style: input.style,
-        disabled: Boolean(input.disabled)
-    };
-}
-
-function getTemplateData(state) {
-    return state;
-}
-
-function handleClick(originalEvent) {
-    if (!this.state.disabled) {
+module.exports = require("marko-widgets").defineComponent({
+    template: require('./template.marko'),
+    handleChange(originalEvent) {
         emitAndFire(this, 'switch-change', {
             originalEvent,
             value: originalEvent.target.value,
             checked: originalEvent.target.checked
         });
     }
-}
-
-module.exports = markoWidgets.defineComponent({
-    template,
-    getInitialState,
-    getTemplateData,
-    handleClick
 });
