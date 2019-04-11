728ab42d3a886b8bab7eb6dd8fdb25671f081366
Dylan Piercey <dpiercey@ebay.com>
2019-04-11 10:28:17 -0700

    Refactor ebay-notice

--- src/components/ebay-notice/index.js
+++ src/components/ebay-notice/index.js
@@ -1,83 +1,25 @@
-const processHtmlAttributes = require('../../common/html-attributes');
 const observer = require('../../common/property-observer');
 const emitAndFire = require('../../common/emit-and-fire');
 const template = require('./template.marko');
 
-const constants = {
-    'inline': {
-        headingTag: 'span',
-        contentTag: 'span',
-        mainTag: 'div'
-    },
-    'page': {
-        headingTag: 'h2',
-        contentTag: 'div',
-        mainTag: 'section'
-    }
-};
-
-const defaults = {
-    type: 'page',
-    status: 'priority',
-    hidden: false,
-    dismissible: false
-};
-
-function getInitialState(input) {
-    const hidden = defaults.hidden;
-    const type = input.type || defaults.type;
-    const a11yHeadingTag = input.a11yHeadingTag;
-    const headingTag = (type === 'page' && a11yHeadingTag) ? a11yHeadingTag : constants[type].headingTag;
-    const status = input.status || defaults.status;
-    const dismissible = (input.dismissible && type === 'page') || defaults.dismissible;
-
-    return {
-        htmlAttributes: processHtmlAttributes(input),
-        mainClass: [`${type}-notice`, `${type}-notice--${status}`, input.class],
-        style: input.style,
-        mainTag: constants[type].mainTag,
-        headingTag: headingTag,
-        contentTag: constants[type].contentTag,
-        dismissible,
-        type,
-        status,
-        hidden,
-        a11yHeadingText: input.a11yHeadingText || '',
-        a11yCloseText: input.a11yCloseText || '',
-        headingClass: `${type}-notice__status`,
-        contentClass: `${type}-notice__content`
-    };
-}
-function getTemplateData(state) {
-    return state;
-}
-
-function init() {
-    observer.observeRoot(this, ['hidden'], () => {
-        this.processAfterStateChange();
-    });
-}
-/**
- * Common processing after data change via both UI and API
- */
-function processAfterStateChange() {
-    if (this.state.hidden) {
-        emitAndFire(this, 'notice-close');
-    } else {
-        emitAndFire(this, 'notice-show');
-    }
-}
-
-function onDismiss() {
-    this.setState('hidden', true);
-    this.processAfterStateChange();
-}
-
 module.exports = require('marko-widgets').defineComponent({
     template,
-    getInitialState,
-    getTemplateData,
-    init,
-    processAfterStateChange,
-    onDismiss
+    getInitialState(input) {
+        return Object.assign({}, input, {
+            hidden: input.hidden || false
+        });
+    },
+    init() {
+        observer.observeRoot(this, ['hidden'], this.setHidden.bind(this));
+    },
+    onDismiss() {
+        this.setHidden(true);
+    },
+    setHidden(hidden) {
+        this.setState("hidden", hidden);
+
+        if (this.isDirty()) {
+            emitAndFire(this, hidden ? 'notice-close' : 'notice-show');
+        }
+    }
 });
