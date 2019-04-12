725952a4ac91bc2d623ecae13dffe00e6a9eea92
Dylan Piercey <dpiercey@ebay.com>
2019-04-11 17:11:16 -0700

    Refactor tooltip

--- src/components/ebay-tooltip/index.js
+++ src/components/ebay-tooltip/index.js
@@ -1,30 +1,11 @@
-const processHtmlAttributes = require('../../common/html-attributes');
 const emitAndFire = require('../../common/emit-and-fire');
-const template = require('./template.marko');
-
-function getInitialState(input) {
-    const noHover = !!input.noHover || false;
-
-    return Object.assign({}, input, {
-        pointer: input.pointer || 'bottom',
-        htmlAttributes: processHtmlAttributes(input),
-        hostSelector: '.tourtip__host',
-        overlaySelector: '.tourtip__overlay',
-        noHover
-    });
-}
-
-function handleExpand() {
-    emitAndFire(this, 'tooltip-expand');
-}
-
-function handleCollapse() {
-    emitAndFire(this, 'tooltip-collapse');
-}
 
 module.exports = require('marko-widgets').defineComponent({
-    template,
-    getInitialState,
-    handleExpand,
-    handleCollapse
+    template: require('./template.marko'),
+    handleExpand() {
+        emitAndFire(this, 'tooltip-expand');
+    },
+    handleCollapse() {
+        emitAndFire(this, 'tooltip-collapse');
+    }
 });
