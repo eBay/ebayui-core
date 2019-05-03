3a1a6c8f46c7fb1c29b0ab6d342112f24769e08a
Ian McBurnie <ianmcburnie@hotmail.com>
2019-05-03 08:42:56 -0700

    Menu: added menuitem shortcut via keypress (#653)

---
+++ src/common/nodelist-utils/index.js
@@ -0,0 +1,8 @@
+// returns index of first node in NodeList with matching first character (case-insenstive)
+function findNodeWithFirstChar(nodeList, char) {
+    return [...nodeList].findIndex((el) => el.innerText.charAt(0).toLowerCase() === char);
+}
+
+module.exports = {
+    findNodeWithFirstChar
+};
