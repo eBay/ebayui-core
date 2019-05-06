df387d10d277ad7a25001f112d3f755c2148d5c9
Ian McBurnie <ianmcburnie@hotmail.com>
2019-05-06 12:14:34 -0700

    Menu: fix shortcut when CAPS-LOCK enabled

--- src/common/nodelist-utils/index.js
+++ src/common/nodelist-utils/index.js
@@ -1,6 +1,6 @@
 // returns index of first node in NodeList with matching first character (case-insenstive)
 function findNodeWithFirstChar(nodeList, char) {
-    return [...nodeList].findIndex((el) => el.innerText.charAt(0).toLowerCase() === char);
+    return [...nodeList].findIndex((el) => el.innerText.charAt(0).toLowerCase() === char.toLowerCase());
 }
 
 module.exports = {
