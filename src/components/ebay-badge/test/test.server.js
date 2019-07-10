755cd73d90a9d818bc5276099a00bf08fe0ab44a
Dylan Piercey <dpiercey@ebay.com>
2019-07-10 12:37:28 -0700

    Conver badge tests to @marko/testing-library

--- src/components/ebay-badge/test/test.server.js
+++ src/components/ebay-badge/test/test.server.js
@@ -1,67 +1,49 @@
-const expect = require('chai').expect;
-const testUtils = require('../../../common/test-utils/server');
+const { expect, use } = require('chai');
+const { render } = require("@marko/testing-library");
+const template = require('../index.marko');
+use(require("chai-dom"));
 
-test('renders defaults', context => {
-    const input = { number: 5 };
-    const $ = testUtils.getCheerio(context.render(input));
-    const badge = $('.badge');
-    expect(badge.length).to.equal(1);
-    expect(badge.text()).to.equal('5');
+
+test('renders defaults', async () => {
+    const { getByText } = await render(template, { number: 5 });
+    expect(getByText(/\d+/)).has.text('5');
 });
 
-test('renders number with rounded-up value', context => {
-    const input = { number: 5.6 };
-    const $ = testUtils.getCheerio(context.render(input));
-    const badge = $('.badge');
-    expect(badge.length).to.equal(1);
-    expect(badge.text()).to.equal('6');
+test('renders number with rounded-up value', async () => {
+    const { getByText } = await render(template, { number: 5.6 });
+    expect(getByText(/\d+/)).has.text('6');
 });
 
-test('does not render with negative value', context => {
-    const input = { number: -5 };
-    const $ = testUtils.getCheerio(context.render(input));
-    const badge = $('.badge');
-    expect(badge.length).to.equal(0);
+test('does not render with negative value', async () => {
+    const { getByText } = await render(template, { number: -5 });
+    expect(() => getByText(/\d+/)).to.throw("Unable to find an element");
 });
 
 describe('given number is a string', () => {
-    test('renders number with coerced string', context => {
-        const input = { number: '5' };
-        const $ = testUtils.getCheerio(context.render(input));
-        const badge = $('.badge');
-        expect(badge.length).to.equal(1);
-        expect(badge.text()).to.equal('5');
+    test('renders number with coerced string', async () => {
+        const { getByText } = await render(template, { number: '5' });
+        expect(getByText(/\d+/)).has.text('5');
     });
 
-    test('renders number with rounded-up string', context => {
-        const input = { number: '5.4' };
-        const $ = testUtils.getCheerio(context.render(input));
-        const badge = $('.badge');
-        expect(badge.length).to.equal(1);
-        expect(badge.text()).to.equal('5');
+    test('renders number with rounded-up string', async () => {
+        const { getByText } = await render(template, { number: '5.4' });
+        expect(getByText(/\d+/)).has.text('5');
     });
 
-    test('does not renders with an invalid string', context => {
-        const input = { number: 'five' };
-        const $ = testUtils.getCheerio(context.render(input));
-        const badge = $('.badge');
-        expect(badge.length).to.equal(0);
+    test('does not renders with an invalid string', async () => {
+        const { getByText } = await render(template, { number: 'five' });
+        expect(() => getByText(/\d+/)).to.throw("Unable to find an element");
     });
 
-    test('does not renders with a negative string', context => {
-        const input = { number: '-5' };
-        const $ = testUtils.getCheerio(context.render(input));
-        const badge = $('.badge');
-        expect(badge.length).to.equal(0);
+    test('does not renders with a negative string', async () => {
+        const { getByText } = await render(template, { number: '-5' });
+        expect(() => getByText(/\d+/)).to.throw("Unable to find an element");
     });
 });
 
-test('truncates when the value is greater than 99', context => {
-    const input = { number: 150 };
-    const $ = testUtils.getCheerio(context.render(input));
-    const badge = $('.badge');
-    expect(badge.length).to.equal(1);
-    expect(badge.text()).to.equal('99+');
+test('truncates when the value is greater than 99', async () => {
+    const { getByText } = await render(template, { number: 150 });
+    expect(getByText(/\d+/)).has.text('99+');
 });
 
 // not sure why these don't work
