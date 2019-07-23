1e4e6f8153340bd0f90ed218cd7e58a749e5d1ba
Dylan Piercey <dpiercey@ebay.com>
2019-07-23 16:58:26 -0700

    Fix nested attribute transforms not running

--- src/common/migrators/tag-name/index.js
+++ src/common/migrators/tag-name/index.js
@@ -2,7 +2,8 @@ const RENAMES = {
     'ebay-listbox': 'ebay-listbox-button',
     'ebay-listbox-option': 'ebay-listbox-button-option',
     'ebay-menu': 'ebay-menu-button',
-    'ebay-menu-option': 'ebay-menu-button-option'
+    'ebay-menu-item': 'ebay-menu-button-item',
+    'ebay-menu-label': 'ebay-menu-button-label'
 };
 
 /**
@@ -19,7 +20,12 @@ function migrate(el, context) {
             el
         );
 
-        el.setTagName(newName);
+        const replacement = context.createNodeForEl(
+            newName,
+            el.getAttributes()
+        );
+        replacement.body = replacement.makeContainer(el.body.items);
+        el.replaceWith(replacement);
     }
 }
 
