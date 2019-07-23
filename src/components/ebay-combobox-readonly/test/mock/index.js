c1cc6c3d0575f478b35eb2bf02519c1a5e3f37da
Dylan Piercey <dpiercey@ebay.com>
2019-07-23 16:30:38 -0700

    Combobox-readyonly server side tests

--- src/components/ebay-combobox-readonly/test/mock/index.js
+++ src/components/ebay-combobox-readonly/test/mock/index.js
@@ -22,7 +22,17 @@ exports.Combobox_3Options = {
 };
 
 exports.Combobox_3Options_2Selected = assign({}, exports.Combobox_3Options, {
-    value: exports.Combobox_3Options.options[1].text
+    options: [{
+        value: '1',
+        text: 'option 1'
+    }, {
+        value: '2',
+        text: 'option 2',
+        selected: true
+    }, {
+        value: '3',
+        text: 'option 3'
+    }]
 });
 
 exports.Combobox_3Options_Borderless = assign({}, exports.Combobox_3Options, {
--- src/components/ebay-combobox-readonly/test/test.server.js
+++ src/components/ebay-combobox-readonly/test/test.server.js
@@ -1,64 +1,70 @@
-const expect = require('chai').expect;
-const testUtils = require('../../../common/test-utils/server');
-const options = [{
-    value: 1,
-    text: 'option 1'
-}, {
-    value: 2,
-    text: 'option 2'
-}];
-const emptyOptions = [];
+const find = require('core-js-pure/features/array/find');
+const findIndex = require('core-js-pure/features/array/find-index');
+const { expect, use } = require('chai');
+const { render } = require('@marko/testing-library');
+const { testPassThroughAttributes } = require('../../../common/test-utils/server');
+const mock = require('./mock');
+const template = require('..');
 
-describe('combobox', () => {
-    it('renders basic version', context => {
-        const input = { options };
-        const $ = testUtils.getCheerio(context.render(input));
-        expect($('.combobox').length).to.equal(1);
-        expect($('.combobox__control').length).to.equal(1);
-        expect($('.combobox__options[role=listbox]').length).to.equal(1);
-        expect($('.combobox__option[role=option]').length).to.equal(2);
-        expect($('.combobox__option[role=option][aria-selected="true"]').length).to.equal(1);
+use(require('chai-dom'));
+
+describe.only('combobox-readonly', () => {
+    it('renders basic version', async() => {
+        const input = mock.Combobox_3Options;
+        const { getByRole, getAllByRole } = await render(template, input);
+        const combobox = find(getAllByRole('combobox'), isVisible);
+        const listbox = find(getAllByRole('listbox'), isVisible);
+        const visibleOptions = getAllByRole('option').filter(isVisible);
+        expect(combobox).has.attr('aria-haspopup');
+        expect(combobox.parentElement).does.not.have.class('combobox__control--borderless');
+        expect(listbox).has.class('combobox__options');
+        expect(listbox).has.property('parentElement').with.class('combobox');
+        expect(visibleOptions).has.length(3);
+        expect(visibleOptions.filter(isAriaSelected)).has.length(1);
     });
 
-    it('renders empty', context => {
-        const input = { emptyOptions };
-        const $ = testUtils.getCheerio(context.render(input));
-        expect($('.combobox').length).to.equal(1);
-        expect($('.combobox__control').length).to.equal(1);
-        expect($('.combobox__options[role=listbox]').length).to.equal(0);
-        expect($('.combobox__option[role=option]').length).to.equal(0);
+    it('renders empty', async() => {
+        const input = mock.Combobox_0Options;
+        const { getAllByRole } = await render(template, input);
+        expect(getAllByRole('combobox')).has.length(2);
+        expect(getAllByRole('listbox')).has.length(1);
     });
 
-    it('renders with second item selected', context => {
-        const input = { options };
-        input.options[0].selected = false;
-        input.options[1].selected = true;
-        const $ = testUtils.getCheerio(context.render(input));
-        expect($('.combobox').length).to.equal(1);
-        expect($('.combobox__control').length).to.equal(1);
-        expect($('.combobox__options[role=listbox]').length).to.equal(1);
-        expect($('.combobox__option[role=option]').length).to.equal(2);
-        expect($('.combobox__option[role=option][aria-selected="true"]:nth-child(2)').length).to.equal(1);
+    it('renders with second item selected', async() => {
+        const input = mock.Combobox_3Options_2Selected;
+        const { getAllByRole } = await render(template, input);
+        expect(findIndex(getAllByRole('option').filter(isVisible), isAriaSelected)).is.equal(1);
     });
 
-    it('renders with borderless=true', context => {
-        const input = { borderless: true, options };
-        const $ = testUtils.getCheerio(context.render(input));
-        expect($('.combobox__control.combobox__control--borderless').length).to.equal(1);
+    it('renders with borderless enabled', async() => {
+        const input = mock.Combobox_3Options_Borderless;
+        const { getAllByRole } = await render(template, input);
+        const combobox = find(getAllByRole('combobox'), isVisible);
+        expect(combobox).has.property('parentElement').with.class('combobox__control--borderless');
     });
 
-    it('renders with borderless=false', context => {
-        const input = { borderless: false, options };
-        const $ = testUtils.getCheerio(context.render(input));
-        expect($('.combobox__control').length).to.equal(1);
-        expect($('.combobox__control.combobox__control--borderless').length).to.equal(0);
+    testPassThroughAttributes(template, {
+        input: mock.Combobox_3Options,
+        getClassAndStyleEl(component) {
+            return component.container.firstElementChild;
+        }
     });
-
-    it('handles pass-through html attributes', context => testUtils.testHtmlAttributes(context, 'input'));
-    it('handles custom class and style', context => testUtils.testClassAndStyle(context, 'span.combobox'));
 });
 
-describe('combobox-option', () => {
-    it('handles pass-through html attributes', c => testUtils.testHtmlAttributes(c, '.combobox__option', 'options'));
-    it('handles custom class and style', c => testUtils.testClassAndStyle(c, '.combobox__option', 'options'));
+describe('combobox-readonly-option', () => {
+    testPassThroughAttributes(template, {
+        child: {
+            name: "options",
+            input: mock.Combobox_3Options.options[0],
+            multiple: true
+        }
+    })
 });
+
+function isAriaSelected(el) {
+    return el.getAttribute('aria-selected') === 'true';
+}
+
+function isVisible(el) {
+    return !el.hasAttribute('hidden') && !el.closest('[hidden]');
+}
