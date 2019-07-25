31dbeebea085badbfc57c2f26cd654a5f1bfb6e4
Andrew Gliga <agliga@ebay.com>
2019-07-25 10:04:10 -0700

    Fixed a lint error

--- src/components/ebay-icon/transformer.js
+++ src/components/ebay-icon/transformer.js
@@ -24,7 +24,8 @@ function transform(el, context) {
         const ds6Path = path.join(iconPath, 'ds6.marko');
         if (!el.hasAttribute('w-id') && context.compilerVersion && context.compilerVersion.indexOf('4.') !== 0) {
             // can be removed in Marko 4
-            el.setAttributeValue('id', builder.expression(`typeof widget !== "undefined" && widget.elId("use_icon_${iconName}")`));
+            el.setAttributeValue('id', 
+                builder.expression(`typeof widget !== "undefined" && widget.elId("use_icon_${iconName}")`));
         }
         el.setAttributeValue('_themes', context.addStaticVar(`icon_${iconName}`, builder.arrayExpression([
             toRequire(ds4Path),
