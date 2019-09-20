1b8b8eec80c631c7b41d82924ce3a684ee073fb5
Dylan Piercey <dpiercey@ebay.com>
2019-09-20 12:57:20 -0700

    Integrate storybook for examples (#874)

---
+++ .storybook/addons.js
@@ -0,0 +1,3 @@
+import 'storybook-readme/register';
+import '@storybook/addon-knobs/register';
+import './plugins/theme-switcher/register';
---
+++ .storybook/webpack.config.js
@@ -0,0 +1,23 @@
+const { BrowserJSONPlugin } = require('webpack-plugin-browser-json');
+
+console.log(process.env.DS);
+
+module.exports = async ({ config }) => {
+  config.devtool = "none";
+  config.resolve.extensions.push('.less');
+
+  config.module.rules.push({
+    test: /\.less$/,
+    use: [
+      'style-loader',
+      'css-loader',
+      'less-loader'
+    ]
+  });
+
+  config.plugins.push(new BrowserJSONPlugin({
+    flags: process.env.DS === "4" ? [] : ['skin-ds6']
+  }));
+
+  return config;
+};
