4b61c76c1286db8f2313ad7667117b749dbd05fb
lrishi <lrishi@ebay.com>
2019-10-22 14:41:41 -0700

    Hide deprecated components from storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure, storiesOf } from "@storybook/marko";
 import { withReadme } from "storybook-readme";
 
 configure(() => {
+  const hiddenStories = ['ebay-combobox-readonly', 'ebay-pill'];
   const requireReadme = require.context("../src", true, /\/README\.md$/);
   const docsByTag = requireReadme.keys().reduce((result, file) => {
     const [, tag] = /([^/]+)\/README\.md$/.exec(file);
@@ -17,12 +18,15 @@ configure(() => {
   requireExample.keys().reduce((storiesByTag, file) => {
     const [, tag, title] = /([^\/]+)\/examples\/\d+-([^\/]+)/.exec(file);
     const component = requireExample(file);
-    (storiesByTag[tag] =
-      storiesByTag[tag] ||
-      storiesOf(tag, module).addDecorator(withReadme(docsByTag[tag]))).add(
-      title,
-      () => ({ component })
-    );
+
+    if (!hiddenStories.includes(tag)) {
+      (storiesByTag[tag] =
+        storiesByTag[tag] ||
+        storiesOf(tag, module).addDecorator(withReadme(docsByTag[tag]))).add(
+          title,
+          () => ({ component })
+        );
+    }
     return storiesByTag;
   }, {});
 }, module);
