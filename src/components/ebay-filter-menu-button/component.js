1cfa88d54d85cc1e4426a823c339906ad51eaa0a
Andrew Gliga <agliga@ebay.com>
2019-11-11 13:58:44 -0800

    Migrated ebay-filter-menu-button

--- src/components/ebay-filter-menu-button/component.js
+++ src/components/ebay-filter-menu-button/component.js
@@ -2,31 +2,8 @@ const assign = require('core-js-pure/features/object/assign');
 const indexOf = require('core-js-pure/features/array/index-of');
 const Expander = require('makeup-expander');
 const eventUtils = require('../../common/event-utils');
-const template = require('./template.marko');
 
-module.exports = require('marko-widgets').defineComponent({
-    template,
-    getInitialState(input) {
-        return assign({}, input, {
-            items: (input.items || []).map(item => assign({}, item))
-        });
-    },
-    onRender() {
-        this.expander = new Expander(this.el, {
-            hostSelector: '.filter-menu-button__button',
-            contentSelector: '.filter-menu-button__menu',
-            focusManagement: 'focusable',
-            expandOnClick: true,
-            autoCollapse: true,
-            alwaysDoFocusManagement: true
-        });
-    },
-    onBeforeUpdate() {
-        this.expander.cancelAsync();
-    },
-    onDestroy() {
-        this.expander.cancelAsync();
-    },
+module.exports = {
     toggleItemChecked(itemEl) {
         const index = indexOf(itemEl.parentNode.children, itemEl);
         const item = this.state.items[index];
@@ -34,33 +11,42 @@ module.exports = require('marko-widgets').defineComponent({
         this.setStateDirty('items');
         this.emitComponentEvent('change', itemEl);
     },
+
     getCheckedValues() {
         return this.state.items
             .filter(item => item.checked)
             .map(item => item.value);
     },
+
     handleMenuKeydown({ originalEvent }) {
         eventUtils.handleEscapeKeydown(originalEvent, () => this.expander.collapse());
     },
+
     handleMenuChange({ el }) {
         this.toggleItemChecked(el);
     },
+
     handleItemKeydown({ el, originalEvent }) {
         eventUtils.handleActionKeydown(originalEvent, () => this.toggleItemChecked(el));
     },
+
     handleFooterButtonClick() {
         this.emitComponentEvent('footer-click');
         this.expander.collapse();
     },
+
     handleFormSubmit({ originalEvent }) {
         this.emitComponentEvent('form-submit', originalEvent);
     },
+
     handleExpand({ originalEvent }) {
         this.emitComponentEvent('expand', null, originalEvent);
     },
+
     handleCollapse({ originalEvent }) {
         this.emitComponentEvent('collapse', null, originalEvent);
     },
+
     emitComponentEvent(eventType, itemEl, originalEvent) {
         switch (eventType) {
             case 'expand':
@@ -83,5 +69,50 @@ module.exports = require('marko-widgets').defineComponent({
                 });
                 break;
         }
+    },
+
+    onInput(input) {
+        this.state = assign({}, input, {
+            items: (input.items || []).map(item => assign({}, item))
+        });
+    },
+
+    onRender() {
+        if (typeof window !== 'undefined') {
+            this._onDestroy();
+        }
+    },
+
+    onMount() {
+        this.onRenderLegacy({
+            firstRender: true
+        });
+    },
+
+    onUpdate() {
+        this.onRenderLegacy({
+            firstRender: false
+        });
+    },
+
+    _onDestroy() {
+        if (this.expander) {
+            this.expander.cancelAsync();
+        }
+    },
+
+    onDestroy() {
+        this._onDestroy();
+    },
+
+    onRenderLegacy() {
+        this.expander = new Expander(this.el, {
+            hostSelector: '.filter-menu-button__button',
+            contentSelector: '.filter-menu-button__menu',
+            focusManagement: 'focusable',
+            expandOnClick: true,
+            autoCollapse: true,
+            alwaysDoFocusManagement: true
+        });
     }
-});
+};
