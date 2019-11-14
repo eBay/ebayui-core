381a4a1cbfb7bfc17b888bbccdb21727ba563f96
Andrew Gliga <agliga@ebay.com>
2019-11-14 11:00:53 -0800

    Migrated ebay-tab

--- src/components/ebay-tab/component.js
+++ src/components/ebay-tab/component.js
@@ -2,41 +2,17 @@ const assign = require('core-js-pure/features/object/assign');
 const rovingTabindex = require('makeup-roving-tabindex');
 const eventUtils = require('../../common/event-utils');
 
-module.exports = require('marko-widgets').defineComponent({
-    template: require('./template.marko'),
-    getInitialProps(input) {
-        return assign({
-            activation: 'auto',
-            headings: [],
-            panels: []
-        }, input);
-    },
-    getInitialState(input) {
-        return assign({}, input, {
-            index: (parseInt(input.index, 10) || 0) % (input.headings.length || 1)
-        });
-    },
-    init() {
-        if (!this.state.fake) {
-            const linearRovingTabindex = rovingTabindex.createLinear(
-                this.getEl('headings'),
-                '.tabs__item',
-                { index: this.state.index }
-            );
-
-            linearRovingTabindex.wrap = true;
-        }
-    },
+module.exports = {
     /**
      * Handle a11y for heading
      * https://ebay.gitbooks.io/mindpatterns/content/disclosure/tabs.html
      * @param {KeyboardEvent} event
      * @param {HTMLDivElement} el
      */
-    handleHeadingKeydown(event, el) {
+    handleHeadingKeydown(dataIndex, event) {
         eventUtils.handleActionKeydown(event, () => {
             event.preventDefault();
-            this.setIndex(el.dataset.index);
+            this.setIndex(dataIndex);
         });
 
         eventUtils.handleArrowsKeydown(event, () => {
@@ -53,9 +29,11 @@ module.exports = require('marko-widgets').defineComponent({
             }
         });
     },
-    handleHeadingClick(_, el) {
-        this.setIndex(el.dataset.index);
+
+    handleHeadingClick(index) {
+        this.setIndex(index);
     },
+
     setIndex(rawIndex) {
         const len = this.state.headings.length;
         const index = ((parseInt(rawIndex, 10) || 0) + len) % len;
@@ -64,5 +42,28 @@ module.exports = require('marko-widgets').defineComponent({
             this.setState('index', index);
             this.emit('tab-select', { index });
         }
+    },
+
+    onInput(input) {
+        const headings = input.headings || [];
+        this.state = assign({
+            activation: 'auto',
+            headings,
+            panels: []
+        }, input, {
+            index: (parseInt(input.index, 10) || 0) % (headings.length || 1)
+        });
+    },
+
+    onMount() {
+        if (!this.state.fake) {
+            const linearRovingTabindex = rovingTabindex.createLinear(
+                this.getEl('headings'),
+                '.tabs__item',
+                { index: this.state.index }
+            );
+
+            linearRovingTabindex.wrap = true;
+        }
     }
-});
+};
