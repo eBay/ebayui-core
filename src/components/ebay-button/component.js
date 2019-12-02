a6c93367e381b2aa3293a419d266758f4b59151c
Dylan Piercey <dpiercey@ebay.com>
2019-12-02 15:17:03 -0800

    refactor ebay-button and improve autocomplete

--- src/components/ebay-button/component.js
+++ src/components/ebay-button/component.js
@@ -1,24 +1,17 @@
-const assign = require('core-js-pure/features/object/assign');
 const eventUtils = require('../../common/event-utils');
 
 module.exports = {
     handleClick(originalEvent) {
-        if (!this.state.disabled) {
+        if (!this.input.disabled) {
             this.emit('button-click', { originalEvent });
         }
     },
 
     handleKeydown(originalEvent) {
         eventUtils.handleEscapeKeydown(originalEvent, () => {
-            if (!this.state.disabled) {
+            if (!this.input.disabled) {
                 this.emit('button-escape', { originalEvent });
             }
-        });
-    },
-
-    onCreate(input) {
-        this.state = assign({}, input, {
-            disabled: input.disabled
         });
     }
 };
