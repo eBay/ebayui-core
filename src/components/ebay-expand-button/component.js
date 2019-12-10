3d9fecb0df31811fd7e0b9ee4a44918872d3b25a
Eamon Caton <ecaton@ebay.com>
2019-12-10 14:38:40 -0800

    fix: reverted used of renderer

--- src/components/ebay-expand-button/component.js
+++ src/components/ebay-expand-button/component.js
@@ -1,20 +1,14 @@
-const assign = require('core-js-pure/features/object/assign');
 const eventUtils = require('../../common/event-utils');
 
 module.exports = {
-    onCreate(input) {
-        this.state = assign({}, input, {
-            disabled: input.disabled
-        });
-    },
     handleClick(originalEvent) {
-        if (!this.state.disabled) {
+        if (!this.input.disabled) {
             this.emit('button-click', { originalEvent });
         }
     },
     handleKeydown(originalEvent) {
         eventUtils.handleEscapeKeydown(originalEvent, () => {
-            if (!this.state.disabled) {
+            if (!this.input.disabled) {
                 this.emit('button-escape', { originalEvent });
             }
         });
