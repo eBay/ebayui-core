2b40f88208cd38b69a443adb7f3854e0658a4ac7
Andrew Gliga <agliga@ebay.com>
2020-01-30 14:23:46 -0800

    Fixed infotip to hide on close button click For #1016

--- src/components/ebay-infotip/component.js
+++ src/components/ebay-infotip/component.js
@@ -4,6 +4,10 @@ module.exports = {
         this.emit('tooltip-expand');
     },
 
+    handleOverlayClose() {
+        this.getComponent('base').collapse();
+    },
+
     handleCollapse() {
         this.getEl('host').focus();
         this.emit('tooltip-collapse');
