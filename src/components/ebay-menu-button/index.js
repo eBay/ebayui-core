4dbb9cd64b34dfd399d94762562a3c5a69b664cd
Andrew Gliga <agliga@ebay.com>
2020-02-18 16:00:54 -0800

    Added an option collapseOnSelect to close menu on option selection (#1065)

--- src/components/ebay-menu-button/index.js
+++ src/components/ebay-menu-button/index.js
@@ -46,6 +46,9 @@ module.exports = require('marko-widgets').defineComponent({
                 eventType: 'change',
                 el: itemEl
             });
+            if (this.state.collapseOnSelect) {
+                this.expander.collapse();
+            }
         } else if (this.state.type !== 'radio') {
             item.checked = !item.checked;
             this.setStateDirty('items');
@@ -53,6 +56,9 @@ module.exports = require('marko-widgets').defineComponent({
                 eventType: this.state.type === 'fake' || !this.state.type ? 'select' : 'change',
                 el: itemEl
             });
+            if (this.state.collapseOnSelect) {
+                this.expander.collapse();
+            }
         }
 
         if (this.rovingTabindex) {
