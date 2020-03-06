f94b6d89f1fdd8a4db4c7a9d5b59be75b13e4888
Andrew Gliga <agliga@ebay.com>
2020-03-06 13:39:22 -0800

    Added details component (#1071)

---
+++ src/components/ebay-details/README.md
@@ -0,0 +1,24 @@
+# ebay-details
+
+```marko
+<ebay-details open text="Show me the details">
+    The details
+</ebay-details>
+```
+
+## Attributes
+
+Name | Type | Stateful | Required | Description
+--- | --- | --- | --- | ---
+`text` | String | No | No | The text to display in the details toggle
+`type` | String | No | No | Can be "regular" / "center". Default "regular"
+`size` | String | No | No | Can be "regular" / "small". Default "regular"
+`open` | Boolean | No | No | Whether details is open.
+`rtl` | Boolean | No | No | Display details on the right side for right to left languages.
+
+## Events
+
+Event | Data | Description
+--- | --- | ---
+`details-toggle` | `{ originalEvent, open }` | details toggled. Open is boolean if true then details was opened, otherwise false means details closed
+`details-click` | `{ originalEvent }` | details clicked
---
+++ src/components/ebay-details/test/test.browser.js
@@ -0,0 +1,131 @@
+const assign = require('core-js-pure/features/object/assign');
+const { expect, use } = require('chai');
+const { render, fireEvent, cleanup } = require('@marko/testing-library');
+const { fastAnimations } = require('../../../common/test-utils/browser');
+const mock = require('./mock');
+const template = require('..');
+
+use(require('chai-dom'));
+before(fastAnimations.start);
+after(fastAnimations.stop);
+afterEach(cleanup);
+let component;
+
+describe('given the details is in the default state', () => {
+    const input = mock.Default_Details;
+
+    beforeEach(async() => {
+        component = await render(template, input);
+    });
+
+    it('should render with open false', () => {
+        expect(component.getByText(input.text).closest('details').open).to.equal(false);
+    });
+});
+
+describe('given the details is in the open state', () => {
+    const input = mock.Open_Details;
+
+    beforeEach(async() => {
+        component = await render(template, input);
+    });
+
+    it('should render with open false', () => {
+        expect(component.getByText(input.text).closest('details').open).to.equal(true);
+    });
+});
+
+describe('given the details is in the default state and click is triggered', () => {
+    const input = mock.Default_Details;
+    const detailsText = input.text;
+
+    beforeEach(async() => {
+        component = await render(template, input);
+    });
+
+    describe('click on details', () => {
+        beforeEach(async() => {
+            await fireEvent.click(component.getByText(detailsText));
+        });
+
+        it('then it emits the details-toggle and click', async() => {
+            verifyToggleEvent();
+            verifyClickEvent();
+        });
+    });
+
+    describe('details should properly toggle open property', () => {
+        beforeEach(async() => {
+            await component.rerender(assign({}, input, { open: true }));
+        });
+
+        it('then it should have open true', () => {
+            expect(component.getByText(detailsText).closest('details').open).to.equal(true);
+        });
+        describe('click after rerender', () => {
+            beforeEach(async() => {
+                await fireEvent.click(component.getByText(detailsText).parentNode);
+            });
+
+            it('then it should be closed', async() => {
+                verifyToggleEvent();
+                verifyClickEvent();
+            });
+        });
+    });
+});
+
+describe('given the details is in the open state and click is triggered', () => {
+    const input = mock.Open_Details;
+    const detailsText = input.text;
+
+    beforeEach(async() => {
+        component = await render(template, input);
+    });
+
+    describe('click on details', () => {
+        beforeEach(async() => {
+            await fireEvent.click(component.getByText(detailsText));
+        });
+
+        it('then it emits the details-toggle and click', async() => {
+            verifyToggleEvent();
+            verifyClickEvent();
+        });
+    });
+
+    describe('details should properly toggle open property', () => {
+        beforeEach(async() => {
+            await component.rerender(assign({}, input, { open: false }));
+        });
+
+        it('then it should have open true', () => {
+            expect(component.getByText(detailsText).closest('details').open).to.equal(false);
+        });
+        describe('click after rerender', () => {
+            beforeEach(async() => {
+                await fireEvent.click(component.getByText(detailsText).parentNode);
+            });
+            it('then it should be open', async() => {
+                verifyToggleEvent();
+                verifyClickEvent();
+            });
+        });
+    });
+});
+
+function verifyToggleEvent() {
+    const toggleEvent = component.emitted('details-toggle');
+    expect(toggleEvent.length).to.be.greaterThan(0);
+
+    const [eventArg] = toggleEvent.pop();
+    expect(eventArg).has.property('open');
+    expect(eventArg).has.property('originalEvent').is.an.instanceOf(Event);
+}
+
+function verifyClickEvent() {
+    const toggleEvent = component.emitted('details-click');
+    expect(toggleEvent).to.length(1);
+    const [[eventArg]] = toggleEvent;
+    expect(eventArg).has.property('originalEvent').is.an.instanceOf(Event);
+}
