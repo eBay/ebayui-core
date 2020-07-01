1e323cfe146366b885e72221edbb4dcbab68e63e
Andrew Gliga <agliga@ebay.com>
2020-07-01 08:27:31 -0700

    Split up listbox to have it's own separate component

--- src/components/ebay-listbox/test/test.browser.js
+++ src/components/ebay-listbox/test/test.browser.js
@@ -22,14 +22,6 @@ describe('given the listbox with 3 items', () => {
         component = await render(template, input, { container: form });
     });
 
-    it('then it should not be expanded', () => {
-        expect(component.getByRole('button')).has.attr('aria-expanded', 'false');
-    });
-
-    it('then it should have button with name attribute', () => {
-        expect(component.getByRole('button')).has.attr('name', 'listbox-button-name');
-    });
-
     it('then the native select should be initialized to the first option value', () => {
         expect(form.elements)
             .has.property(input.name)
@@ -38,55 +30,37 @@ describe('given the listbox with 3 items', () => {
 
     describe('when the down arrow key is pressed', () => {
         beforeEach(async() => {
-            await pressKey(component.getByRole('button'), {
+            await pressKey(component.getAllByRole('listbox').find(isVisible), {
                 key: 'ArrowDown',
                 keyCode: 40
             });
         });
 
-        it('then it should not expand the listbox', () => {
-            expect(component.getByRole('button')).has.attr('aria-expanded', 'false');
+        it('then it emits the listbox-change event with the correct data', () => {
+            const changeEvents = component.emitted('listbox-change');
+            expect(changeEvents).has.length(1);
+
+            const [[changeEvent]] = changeEvents;
+            expect(changeEvent).has.property('index', 1);
+            expect(changeEvent).has.property('selected').and.is.deep.equal([input.options[1].value]);
         });
 
         describe('when the up arrow key is pressed', () => {
             beforeEach(async() => {
                 component.emitted('listbox-change');
-                await pressKey(component.getByRole('button'), {
+                await pressKey(component.getAllByRole('listbox').find(isVisible), {
                     key: 'ArrowUp',
                     keyCode: 38
                 });
             });
 
-            it('then it should not expand the listbox', () => {
-                expect(component.getByRole('button')).has.attr('aria-expanded', 'false');
-            });
-        });
-    });
+            it('then it emits the listbox-change event with the correct data', () => {
+                const changeEvents = component.emitted('listbox-change');
+                expect(changeEvents).has.length(1);
 
-    describe('when the button is clicked', () => {
-        beforeEach(async() => {
-            await fireEvent.click(component.getByRole('button'));
-        });
-
-        it('then it emits the event from expander-expand', () => {
-            expect(component.emitted('listbox-expand')).has.length(1);
-        });
-
-        it('then it has expanded the listbox', () => {
-            expect(component.getByRole('button')).has.attr('aria-expanded', 'true');
-        });
-
-        describe('when the button is clicked again', () => {
-            beforeEach(async() => {
-                await fireEvent.click(component.getByRole('button'));
-            });
-
-            it('then it emits the event from expander-collapse', () => {
-                expect(component.emitted('listbox-collapse')).has.length(1);
-            });
-
-            it('then it has collapsed the listbox', () => {
-                expect(component.getByRole('button')).has.attr('aria-expanded', 'false');
+                const [[changeEvent]] = changeEvents;
+                expect(changeEvent).has.property('index', 0);
+                expect(changeEvent).has.property('selected').and.is.deep.equal([input.options[0].value]);
             });
         });
     });
@@ -97,7 +71,6 @@ describe('given the listbox is in an expanded state', () => {
 
     beforeEach(async() => {
         component = await render(template, input, { container: form });
-        await fireEvent.click(component.getByRole('button'));
     });
 
     describe('when an option is clicked', () => {
@@ -105,7 +78,7 @@ describe('given the listbox is in an expanded state', () => {
             await fireEvent.click(component.getByText(input.options[1].text));
         });
 
-        it('Should trigger listbox change change', () => {
+        it('then it emits the listbox-change event with correct data', () => {
             const changeEvents = component.emitted('listbox-change');
             expect(changeEvents).has.length(1);
 
@@ -130,6 +103,25 @@ describe('given the listbox is in an expanded state', () => {
             const [[changeEvent]] = changeEvents;
             expect(changeEvent).has.property('index', 1);
             expect(changeEvent).has.property('selected').and.is.deep.equal([input.options[1].value]);
+        });
+
+        describe('when the up arrow key is pressed', () => {
+            beforeEach(async() => {
+                component.emitted('listbox-change');
+                await pressKey(component.getAllByRole('listbox').find(isVisible), {
+                    key: 'ArrowUp',
+                    keyCode: 38
+                });
+            });
+
+            it('then it emits the listbox-change event with the correct data', () => {
+                const changeEvents = component.emitted('listbox-change');
+                expect(changeEvents).has.length(1);
+
+                const [[changeEvent]] = changeEvents;
+                expect(changeEvent).has.property('index', 0);
+                expect(changeEvent).has.property('selected').and.is.deep.equal([input.options[0].value]);
+            });
         });
     });
 });
--- src/components/ebay-listbox/test/test.server.js
+++ src/components/ebay-listbox/test/test.server.js
@@ -9,18 +9,12 @@ use(require('chai-dom'));
 describe('listbox', () => {
     it('renders basic version', async() => {
         const input = mock.Basic_3Options;
-        const { getByRole, getAllByRole } = await render(template, input);
+        const { getAllByRole } = await render(template, input);
 
-        const btnEl = getByRole('button');
         const listboxEl = getAllByRole('listbox').find(isVisible);
         const visibleOptionEls = getAllByRole('option').filter(isVisible);
 
-        expect(btnEl).has.attr('aria-haspopup', 'listbox');
-        expect(btnEl).has.attr('name', input.buttonName);
-        expect(btnEl).has.text(input.options[0].text);
-        expect(btnEl).has.class('listbox-button__control');
-
-        expect(listboxEl).has.class('listbox-button__listbox');
+        expect(listboxEl).has.class('listbox__options');
 
         expect(visibleOptionEls).has.length(3);
         visibleOptionEls.forEach((optionEl, i) => {
@@ -35,15 +29,7 @@ describe('listbox', () => {
     it('renders empty', async() => {
         const input = mock.Basic_0Options;
         const { getAllByRole } = await render(template, input);
-        expect(getAllByRole('button')).has.length(1);
         expect(getAllByRole('listbox').filter(isVisible)[0].childNodes).has.length(0);
-    });
-
-    it('renders fluid layout', async() => {
-        const input = mock.Basic_3Options_fluid;
-        const { getAllByRole } = await render(template, input);
-        expect(getAllByRole('button')).has.length(1);
-        expect(getAllByRole('listbox')[0].parentNode).has.class('listbox-button--fluid');
     });
 
     it('renders with second item selected', async() => {
