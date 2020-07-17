cce181bf8ca3c84bd18440fd097a7f7e6434f682
Andrew Gliga <agliga@ebay.com>
2020-07-17 12:08:41 -0700

    Added inline notice component

--- src/components/ebay-inline-notice/test/mock/index.js
+++ src/components/ebay-inline-notice/test/mock/index.js
@@ -2,12 +2,9 @@ const assign = require('core-js-pure/features/object/assign');
 const { createRenderBody } = require('../../../../common/test-utils/shared');
 
 exports.Inline = {
-    a11yHeadingText: 'Heading Text',
+    a11yText: 'Heading Text',
     renderBody: createRenderBody('Content')
 };
-exports.Inline_Custom_Heading_Tag = assign({}, exports.Inline, {
-    a11yHeadingTag: 'h3'
-});
 
 exports.Inline_Custom_Status = assign({}, exports.Inline, {
     status: 'confirmation'
--- src/components/ebay-inline-notice/test/test.server.js
+++ src/components/ebay-inline-notice/test/test.server.js
@@ -1,6 +1,5 @@
 const { expect, use } = require('chai');
 const { render } = require('@marko/testing-library');
-const assign = require('core-js-pure/features/object/assign');
 const { testPassThroughAttributes } = require('../../../common/test-utils/server');
 const template = require('..');
 const mock = require('./mock');
@@ -11,7 +10,7 @@ describe('inline-notice', () => {
     it('renders with defaults', async() => {
         const input = mock.Inline;
         const { getByLabelText, getByText } = await render(template, input);
-        const status = getByLabelText(input.a11yHeadingText).parentElement;
+        const status = getByLabelText(input.a11yText).parentElement;
         expect(status).has.class('inline-notice__header');
         expect(status).has.property('tagName', 'SPAN');
         expect(status.parentElement).has.class('inline-notice--attention');
@@ -21,17 +20,10 @@ describe('inline-notice', () => {
         expect(content).has.class('inline-notice__main');
     });
 
-    it('renders with custom heading tag (ignores it)', async() => {
-        const input = mock.Inline_Custom_Heading_Tag;
-        const { getByLabelText } = await render(template, input);
-        const status = getByLabelText(input.a11yHeadingText).parentElement;
-        expect(status).has.property('tagName', 'SPAN');
-    });
-
     it('renders with custom status type', async() => {
         const input = mock.Inline_Custom_Status;
         const { getByLabelText } = await render(template, input);
-        const status = getByLabelText(input.a11yHeadingText).parentElement;
+        const status = getByLabelText(input.a11yText).parentElement;
         expect(status.parentElement).has.class(`inline-notice--${input.status}`);
     });
 
