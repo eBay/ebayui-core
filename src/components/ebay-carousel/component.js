25eabfe7e47898d4f0e32fc93a376d4a9b3da01f
Andrew Gliga <agliga@ebay.com>
2020-09-03 10:33:20 -0700

    Added carousel header and title to next/prev buttons

--- src/components/ebay-carousel/component.js
+++ src/components/ebay-carousel/component.js
@@ -19,13 +19,24 @@ function getTemplateData(state) {
     const prevControlDisabled = isSingleSlide || !autoplayInterval && offset === 0;
     const nextControlDisabled = isSingleSlide || !autoplayInterval && offset === getMaxOffset(state);
     const bothControlsDisabled = prevControlDisabled && nextControlDisabled;
-    let slide, itemWidth, totalSlides, a11yStatusText;
+    let slide,
+        itemWidth,
+        totalSlides,
+        a11yStatusText,
+        a11yPreviousText = state.a11yPreviousText,
+        a11yNextText = state.a11yNextText;
 
     if (itemsPerSlide) {
         const itemsInSlide = itemsPerSlide + state.peek;
         slide = getSlide(state);
         itemWidth = `calc(${100 / itemsInSlide}% - ${(itemsInSlide - 1) * gap / itemsInSlide}px)`;
         totalSlides = getSlide(state, items.length);
+        a11yPreviousText = a11yPreviousText
+            .replace('{currentSlide}', slide + 1)
+            .replace('{totalSlides}', totalSlides);
+        a11yNextText = a11yNextText
+            .replace('{currentSlide}', slide + 1)
+            .replace('{totalSlides}', totalSlides);
         a11yStatusText = state.a11yStatusText
             .replace('{currentSlide}', slide + 1)
             .replace('{totalSlides}', totalSlides);
@@ -59,6 +70,8 @@ function getTemplateData(state) {
         slide,
         offset: hasOverride ? config.offsetOverride : offset,
         disableTransition: hasOverride,
+        a11yNextText,
+        a11yPreviousText,
         totalSlides,
         a11yStatusText,
         prevControlDisabled,
@@ -440,7 +453,12 @@ module.exports = {
     togglePlay,
 
     onInput(input) {
+        let titleText = input.a11yHeadingText || input.a11yStatusText;
+        if (titleText) {
+            titleText = ` - ${titleText}`;
+        }
         const gap = parseInt(input.gap, 10);
+
         const state = {
             htmlAttributes: processHtmlAttributes(input, [
                 'class',
@@ -468,8 +486,8 @@ module.exports = {
             gap: isNaN(gap) ? 16 : gap,
             index: parseInt(input.index, 10) || 0,
             itemsPerSlide: parseFloat(input.itemsPerSlide, 10) || undefined,
-            a11yPreviousText: input.a11yPreviousText || 'Previous Slide',
-            a11yNextText: input.a11yNextText || 'Next Slide',
+            a11yPreviousText: input.a11yPreviousText || `'Previous Slide${titleText}`,
+            a11yNextText: input.a11yNextText || `Next Slide${titleText}`,
             a11yStatusText: input.a11yStatusText || 'Showing Slide {currentSlide} of {totalSlides} - Carousel',
             a11yStatusTag: input.a11yStatusTag || 'span',
             a11yHeadingText: input.a11yHeadingText,
