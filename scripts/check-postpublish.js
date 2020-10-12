1b7e1053ae33a0cce58f1e52e7362462ed83258d
Andrew Gliga <agliga@ebay.com>
2020-10-12 09:32:19 -0700

    Added check-postpublish script to build

---
+++ scripts/check-postpublish.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+const path = require('path');
+const execSync = require('child_process').execSync;
+const rootDir = path.join(__dirname, '..');
+
+async function readMarkoJSON() {
+    return await fs.promises.readFile(path.join(rootDir, 'marko.json'), 'utf-8');
+}
+
+async function run() {
+    const preContent = await readMarkoJSON();
+
+    execSync('yarn postpublish');
+
+    const postContent = await readMarkoJSON();
+    if (preContent !== postContent) {
+        console.error('marko.json file changed, publish files are checked in. Run "yarn postpublish" to fix.');
+        process.exit(1);
+    }
+}
+
+run();
