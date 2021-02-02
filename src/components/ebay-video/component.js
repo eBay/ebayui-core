aac3a60bba42799add5a3ee2472c0a7d15977c85
Andrew Gliga <agliga@ebay.com>
2021-02-02 10:29:47 -0800

    Added video loading post page load

--- src/components/ebay-video/component.js
+++ src/components/ebay-video/component.js
@@ -1,16 +1,24 @@
-const mediaPlayer = require('dashjs').MediaPlayer;
+const loader = require('./loader');
 
 module.exports = {
     isIos() {
         return this.input.url && this.input.url.indexOf('.m3u8') > -1;
     },
+    onCreate() {
+        this.state = {
+            loading: true
+        };
+    },
 
     onMount() {
         if (!this.isIos()) {
             this.videoEl = this.getEl('video');
-
-            this.player = mediaPlayer().create();
-            this.player.initialize(this.getEl('video'), this.input.url, true);
+            loader('http://cdn.dashjs.org/v3.1.0/dash.all.min.js').then(() => {
+                this.state.loading = false;
+                this.player = dashjs.MediaPlayer().create();
+                this.player.initialize(this.getEl('video'), this.input.url, true);
+            });
         }
     }
+
 };
---
+++ src/components/ebay-video/loader.js
@@ -0,0 +1,22 @@
+const videoPromise = [];
+
+function load(src) {
+    if (!videoPromise[src]) {
+        videoPromise[src] = new Promise((resolve, reject) => {
+            const head = document.head || document.getElementsByTagName('head')[0];
+            const script = document.createElement('script');
+            script.src = src;
+
+            script.onload = () => {
+                resolve();
+            };
+            script.onerror = () => {
+                reject();
+            };
+            head.appendChild(script);
+        });
+    }
+    return videoPromise[src];
+}
+
+module.exports = load;
