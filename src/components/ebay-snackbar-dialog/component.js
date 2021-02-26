ed58d0bf87fbb3168b7ba0c51c189eb53f42187c
Austin Gardner <gardneraustin@protonmail.com>
2021-02-25 21:18:16 -0800

    snackbar does not close on hover or keyboard focus, but need new selector

--- src/components/ebay-snackbar-dialog/component.js
+++ src/components/ebay-snackbar-dialog/component.js
@@ -2,15 +2,46 @@ const DEFAULT_TIMEOUT_LENGTH = 6000;
 module.exports = {
     onInput(input) {
         clearTimeout(this.timeout);
+        clearInterval(this.interval);
         if (input.open !== false) {
             clearTimeout(this.timeout);
             const timeout = setTimeout(() => {
-                this.emit('close');
+                if (
+                    document
+                        .querySelector('.fake-link.snackbar-dialog__cta.btn.btn--secondary')
+                        .isEqualNode(document.activeElement) ||
+                    Array.from(document.querySelectorAll(':hover')).includes(
+                        document.querySelector('.fake-link.snackbar-dialog__cta.btn.btn--secondary')
+                    )
+                ) {
+                    this.interval = setInterval(() => {
+                        if (
+                            !(
+                                document
+                                    .querySelector(
+                                        '.fake-link.snackbar-dialog__cta.btn.btn--secondary'
+                                    )
+                                    .isEqualNode(document.activeElement) ||
+                                Array.from(document.querySelectorAll(':hover')).includes(
+                                    document.querySelector(
+                                        '.fake-link.snackbar-dialog__cta.btn.btn--secondary'
+                                    )
+                                )
+                            )
+                        ) {
+                            this.emit('close');
+                            clearInterval(this.interval);
+                        }
+                    }, 1000);
+                } else {
+                    this.emit('close');
+                }
             }, input.timeout || DEFAULT_TIMEOUT_LENGTH);
             this.timeout = timeout;
         }
     },
     onDestroy() {
         clearTimeout(this.timeout);
+        clearInterval(this.interval);
     },
 };
