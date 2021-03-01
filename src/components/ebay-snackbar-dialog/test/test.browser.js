24c83c8a05a107d0bcd6d1a913c1d00171a844ef
Austin Gardner <gardneraustin@protonmail.com>
2021-03-01 15:23:08 -0800

    a little bit of test coverage, more in separate PR

--- src/components/ebay-snackbar-dialog/test/test.browser.js
+++ src/components/ebay-snackbar-dialog/test/test.browser.js
@@ -1,4 +1,3 @@
-const assign = require('core-js-pure/features/object/assign');
 const { expect, use } = require('chai');
 const { render, fireEvent, waitFor, cleanup } = require('@marko/testing-library');
 const basicSnackbarTemplate = require('./mock/mock-basic.marko');
@@ -6,16 +5,12 @@ const actionSnackbarTemplate = require('./mock/action-snackbar-template.marko');
 
 use(require('chai-dom'));
 
-// test('renders a message', async () => {
-//   const { container, getByText } = await render(basicSnackbarTemplate);
-//   expect(getByText(/Marko/)).toBeInTheDocument()
-
 /** @type import("@marko/testing-library").RenderResult */
 let component;
 
 describe('given a closed action snackbar', () => {
     beforeEach(async () => {
-        component = await render(actionSnackbarTemplate, { open: false });
+        component = await render(actionSnackbarTemplate);
     });
 
     it('it is not visible in the DOM', () => {
@@ -24,35 +19,21 @@ describe('given a closed action snackbar', () => {
         ).to.have.attribute('hidden');
     });
 
-    it('once the button is clicked it becomes visible', async () => {
-        await fireEvent.click(component.getAllByRole('button'));
-        expect(component.getByRole('dialog')).does.not.have.attribute('hidden');
+    it.skip('once the button is clicked it becomes visible', async () => {
+        await fireEvent.click(component.getByText('Open Action Snackbar'));
+        const visibleSnackbar = await component.findByText(/This is the action snackbar/i);
+        expect(visibleSnackbar.parentElement.parentElement).does.not.have.attribute('hidden');
     });
 });
 
 describe('given an open snackbar', () => {
-    // let sibling;
-
     beforeEach(async () => {
-        component = await render(basicSnackbarTemplate, { open: true });
+        component = await render(basicSnackbarTemplate);
     });
-
-    // afterEach(() => {
-    //     document.body.removeChild(sibling);
-    // });
 
     it('then it is visible in the DOM', () => {
-        expect(component.getByRole('dialog')).does.not.have.attribute('hidden');
+        expect(
+            component.getByText('This is the basic snackbar').parentElement.parentElement
+        ).does.not.have.attribute('hidden');
     });
-
-    // describe('when the mask is clicked', () => {
-    //     beforeEach(async() => {
-    //         // simulate clicking outside the dialog.
-    //         await fireEvent.click(component.getByRole('snackbardialog'));
-    //     });
-
-    //     it('then it is still open in the DOM', async() => {
-    //         await waitFor(() => expect(component.getByRole('snackbardialog')).does.not.have.attr('hidden'));
-    //     });
-    // });
 });
