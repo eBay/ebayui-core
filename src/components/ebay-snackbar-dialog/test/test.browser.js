e7d84177e816832a2e64a94befc313fa83ca8719
Austin Gardner <gardneraustin@protonmail.com>
2021-03-02 10:26:50 -0800

    rest of tests

--- src/components/ebay-snackbar-dialog/test/test.browser.js
+++ src/components/ebay-snackbar-dialog/test/test.browser.js
@@ -19,10 +19,77 @@ describe('given a closed action snackbar', () => {
         ).to.have.attribute('hidden');
     });
 
-    it.skip('once the button is clicked it becomes visible', async () => {
+    it('once the button is clicked it becomes visible', async () => {
         await fireEvent.click(component.getByText('Open Action Snackbar'));
         const visibleSnackbar = await component.findByText(/This is the action snackbar/i);
-        expect(visibleSnackbar.parentElement.parentElement).does.not.have.attribute('hidden');
+        setTimeout(() => {
+            expect(visibleSnackbar.parentElement.parentElement).does.not.have.attribute('hidden');
+        }, 1000);
+    });
+
+    it('then clicking the undo button makes the snackbar disappear', async () => {
+        await fireEvent.click(component.getByText('Open Action Snackbar'));
+        const snackbar = await component.findByText(/This is the action snackbar/i);
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).does.not.have.attribute('hidden');
+        }, 1000);
+        await fireEvent.click(component.getByText(/undo/i));
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).to.have.attribute('hidden');
+        }, 1000);
+    });
+
+    it('mouseEnter prevents the snackbar from disappearing', async () => {
+        await fireEvent.click(component.getByText('Open Action Snackbar'));
+        const snackbar = await component.findByText(/This is the action snackbar/i);
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).does.not.have.attribute('hidden');
+        }, 1000);
+        await fireEvent.mouseEnter(snackbar.parentElement.parentElement);
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).does.not.have.attribute('hidden');
+        }, 7000);
+        await fireEvent.mouseLeave(snackbar.parentElement.parentElement);
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).to.have.attribute('hidden');
+        }, 7000);
+    });
+
+    it('button focus prevents the snackbar from disappearing', async () => {
+        await fireEvent.click(component.getByText('Open Action Snackbar'));
+        const snackbar = await component.findByText(/This is the action snackbar/i);
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).does.not.have.attribute('hidden');
+        }, 1000);
+        await fireEvent.focus(component.getByText(/undo/i));
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).does.not.have.attribute('hidden');
+        }, 7000);
+        await fireEvent.blur(component.getByText(/undo/i));
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).to.have.attribute('hidden');
+        }, 7000);
+    });
+
+    it('mouseEnter, focus, blur still has snackbar visible until after mouseLeave', async () => {
+        await fireEvent.click(component.getByText('Open Action Snackbar'));
+        const snackbar = await component.findByText(/This is the action snackbar/i);
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).does.not.have.attribute('hidden');
+        }, 1000);
+        await fireEvent.mouseEnter(snackbar.parentElement.parentElement);
+        await fireEvent.focus(component.getByText(/undo/i));
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).does.not.have.attribute('hidden');
+        }, 7000);
+        await fireEvent.blur(component.getByText(/undo/i));
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).does.not.have.attribute('hidden');
+        }, 7000);
+        await fireEvent.mouseLeave(snackbar.parentElement.parentElement);
+        setTimeout(() => {
+            expect(snackbar.parentElement.parentElement).to.have.attribute('hidden');
+        }, 7000);
     });
 });
 
