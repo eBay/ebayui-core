3cf057a39d9485285a4b73dff6c3de454f4cee6b
Andrew Gliga <agliga@ebay.com>
2021-03-24 14:02:45 -0700

    Removed aria-haspopup from fake menu (#1349)

--- src/components/ebay-fake-menu-button/test/test.server.js
+++ src/components/ebay-fake-menu-button/test/test.server.js
@@ -8,55 +8,59 @@ const mock = require('./mock');
 use(require('chai-dom'));
 
 describe('fake-menu-button', () => {
-    it('renders fake version', async() => {
+    it('renders fake version', async () => {
         const input = mock.Basic_2Items;
         const { getByText } = await render(template, input);
 
-        input.items.forEach(item => {
-            expect(getByText(item.renderBody.text).closest('.fake-menu-button__item'))
-                .has.attr('href', item.href);
+        input.items.forEach((item) => {
+            expect(getByText(item.renderBody.text).closest('.fake-menu-button__item')).has.attr(
+                'href',
+                item.href
+            );
         });
     });
 
-    it('renders with type=fake, reverse=true', async() => {
+    it('renders with type=fake, reverse=true', async () => {
         const input = assign({ type: 'fake', reverse: true }, mock.Basic_2Items);
         const { getByText } = await render(template, input);
-        expect(getByText(input.items[0].renderBody.text).closest('.fake-menu-button__menu--reverse'))
-            .does.not.equal(null);
+        expect(
+            getByText(input.items[0].renderBody.text).closest('.fake-menu-button__menu--reverse')
+        ).does.not.equal(null);
     });
 
-    it('renders with type=fake, fix-width=true', async() => {
+    it('renders with type=fake, fix-width=true', async () => {
         const input = assign({ type: 'fake', fixWidth: true }, mock.Basic_2Items);
         const { getByText } = await render(template, input);
-        expect(getByText(input.items[0].renderBody.text).closest('.fake-menu-button__menu--fix-width'))
-            .does.not.equal(null);
+        expect(
+            getByText(input.items[0].renderBody.text).closest('.fake-menu-button__menu--fix-width')
+        ).does.not.equal(null);
     });
 
-    it('renders with borderless=true', async() => {
+    it('renders with borderless=true', async () => {
         const input = assign({ borderless: true }, mock.Basic_2Items);
         const { getByRole } = await render(template, input);
         expect(getByRole('button')).has.class('expand-btn--borderless');
     });
 
-    it('renders with size=small', async() => {
+    it('renders with size=small', async () => {
         const input = assign({ size: 'small' }, mock.Basic_2Items);
         const { getByRole } = await render(template, input);
         expect(getByRole('button')).has.class('expand-btn--small');
     });
 
-    it('renders with priority=primary', async() => {
+    it('renders with priority=primary', async () => {
         const input = assign({ priority: 'primary' }, mock.Basic_2Items);
         const { getByRole } = await render(template, input);
         expect(getByRole('button')).has.class('expand-btn--primary');
     });
 
-    it('renders without text', async() => {
+    it('renders without text', async () => {
         const input = assign({}, mock.Basic_2Items, { text: '' });
         const { getByRole } = await render(template, input);
         expect(getByRole('button')).has.class('expand-btn--icon-only');
     });
 
-    it('renders with icon', async() => {
+    it('renders with icon', async () => {
         const input = mock.Settings_Icon;
         const { getByRole, getByText } = await render(template, input);
         const btnEl = getByRole('button');
@@ -64,19 +68,19 @@ describe('fake-menu-button', () => {
         expect(btnEl).contains(getByText(input.icon.renderBody.text));
     });
 
-    it('renders without toggle icon', async() => {
+    it('renders without toggle icon', async () => {
         const input = mock.No_Toggle_Icon;
         const { getByRole } = await render(template, input);
         expect(getByRole('button').querySelector('icon--dropdown')).equals(null);
     });
 
-    it('renders with disabled state', async() => {
+    it('renders with disabled state', async () => {
         const input = mock.Disabled;
         const { getByRole } = await render(template, input);
         expect(getByRole('button')).has.attr('disabled');
     });
 
-    it('renders with a custom label', async() => {
+    it('renders with a custom label', async () => {
         const input = mock.Custom_Label;
         const { getByRole, getByText } = await render(template, input);
         const customLabelEl = getByText(input.label.renderBody.text);
@@ -84,19 +88,18 @@ describe('fake-menu-button', () => {
         expect(getByRole('button')).contains(customLabelEl);
     });
 
-    it('renders with overflow variant', async() => {
+    it('renders with overflow variant', async () => {
         const input = mock.Overflow_Variant;
         const { getByRole, getByLabelText } = await render(template, input);
         const btnEl = getByRole('button');
         expect(btnEl).is.equal(getByLabelText(input.a11yText));
         expect(btnEl).has.class('icon-btn');
-        expect(btnEl).has.attr('aria-haspopup', 'true');
         expect(btnEl).has.attr('aria-expanded', 'false');
         expect(btnEl).has.property('parentElement').with.class('fake-menu-button');
         expect(btnEl.querySelector('.icon')).has.property('tagName', 'svg');
     });
 
-    it('renders with separators', async() => {
+    it('renders with separators', async () => {
         const input = mock.Separator_4Items;
         const { queryByText, getAllByRole } = await render(template, input);
         const separators = getAllByRole('separator');
@@ -114,7 +117,7 @@ describe('fake-menu-button', () => {
     testUtils.testPassThroughAttributes(template, {
         child: {
             name: 'items',
-            multiple: true
-        }
+            multiple: true,
+        },
     });
 });
