18b1949f57c59a3af6ccaa03997b2b0c1153b811
Andrew Gliga <agliga@ebay.com>
2021-04-09 16:38:50 -0700

    feat: new EEK component (#1365)

---
+++ src/components/ebay-eek/README.md
@@ -0,0 +1,22 @@
+<h1 style="display: flex; justify-content: space-between; align-items: center;">
+    <span>
+        ebay-eek
+    </span>
+    <span style="font-weight: normal; font-size: medium; margin-bottom: -15px;">
+        DS v1.0.0
+    </span>
+</h1>
+
+## ebay-eek Usage
+
+```marko
+<ebay-eek rating="A+++" max="A+++" min="D"/>
+```
+
+## ebay-eek Attributes
+
+| Name     | Type   | Stateful | Required | Description       |
+| -------- | ------ | -------- | -------- | ----------------- |
+| `rating` | String | No       | Yes      | The energy rating |
+| `max`    | String | No       | Yes      | The maximum range |
+| `min`    | String | No       | Yes      | The minimum range |
---
+++ src/components/ebay-eek/test/test.server.js
@@ -0,0 +1,77 @@
+const { expect, use } = require('chai');
+const { render } = require('@marko/testing-library');
+const { testPassThroughAttributes } = require('../../../common/test-utils/server');
+const template = require('..');
+
+use(require('chai-dom'));
+
+describe('eek', () => {
+    async function ratingCheck(max, min, rating, number) {
+        const eekClass = !!number ? `eek--rating-${number}` : 'eek';
+        const { getByRole } = await render(template, { max, min, rating });
+        expect(getByRole('figure')).has.class(eekClass);
+    }
+
+    it('renders default eek', async () => {
+        const { getByRole, getByText } = await render(template, {
+            max: 'A+++',
+            min: 'D',
+            rating: 'B',
+        });
+        expect(getByText('A+++')).has.property('nextElementSibling');
+        expect(getByText('D')).has.property('previousElementSibling');
+        expect(getByText('B')).has.class('eek__rating');
+        expect(getByRole('figure')).has.class('eek--rating-5');
+    });
+
+    it('renders invalid eek', async () => {
+        const { getByRole, getByText } = await render(template, {
+            max: 'A',
+            min: 'D',
+            rating: 'B',
+        });
+        expect(getByText('A')).has.property('nextElementSibling');
+        expect(getByText('D')).has.property('previousElementSibling');
+        expect(getByText('B')).has.class('eek__rating');
+        expect(getByRole('figure')).has.class('eek');
+    });
+
+    it('renders the correct eek if rating is outside', async () => {
+        await ratingCheck('A+', 'D', 'A++');
+    });
+
+    it('renders rating 1', async () => {
+        await ratingCheck('A++', 'E', 'A++', '1');
+    });
+
+    it('renders rating 2', async () => {
+        await ratingCheck('A++', 'E', 'A+', '2');
+    });
+
+    it('renders rating 3', async () => {
+        await ratingCheck('A++', 'E', 'A', '3');
+    });
+
+    it('renders rating 4', async () => {
+        await ratingCheck('A++', 'E', 'B', '4');
+    });
+
+    it('renders rating 5', async () => {
+        await ratingCheck('A++', 'E', 'C', '5');
+    });
+
+    it('renders rating 6', async () => {
+        await ratingCheck('A++', 'E', 'D', '6');
+    });
+
+    it('renders rating 7', async () => {
+        await ratingCheck('A++', 'E', 'E', '7');
+    });
+
+    it('renders rating 7 (not 8)', async () => {
+        await ratingCheck('A++', 'G', 'F', '7');
+        await ratingCheck('A++', 'G', 'G', '7');
+    });
+
+    testPassThroughAttributes(template);
+});
