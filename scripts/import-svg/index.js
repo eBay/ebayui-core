2ea0359f502a04025dc5cda08ced96ff813c11d1
Andrew Gliga <agliga@ebay.com>
2021-05-07 13:44:38 -0700

    feat: added program badges and imported new icons

--- scripts/import-svg/index.js
+++ scripts/import-svg/index.js
@@ -15,17 +15,19 @@ const svgDir = path.join(skinDir, 'src/svg');
 const outputDir = path.join(__dirname, '../../src/components/ebay-icon/icons');
 const markoTagJson = require('../../src/components/ebay-icon/marko-tag.json');
 const icons = new Map();
+const programIcons = new Map();
 
 const THEME_NAMES = util.dsIconThemes;
 const htmlMinifierOptions = {
     keepClosingSlash: true,
     removeAttributeQuotes: true,
     removeOptionalTags: true,
-    collapseWhitespace: true
+    collapseWhitespace: true,
 };
 
 const missingSVG = JSON.stringify(
-    minify(fs.readFileSync(path.join(__dirname, 'missing.svg'), 'utf-8'), htmlMinifierOptions));
+    minify(fs.readFileSync(path.join(__dirname, 'missing.svg'), 'utf-8'), htmlMinifierOptions)
+);
 cp.execSync(`rm -rf ${JSON.stringify(outputDir)}`);
 fs.mkdirSync(outputDir);
 
@@ -35,61 +37,83 @@ delete markoTagJson.transformer;
 delete markoTagJson['@_name'];
 delete markoTagJson['@_themes'];
 
-for (const theme of THEME_NAMES) {
-    const svgFile = path.join(svgDir, theme, 'icons.svg');
-    const svgContent = fs.readFileSync(svgFile, 'utf-8');
-    const $ = cheerio.load(svgContent);
+function addIcons(fileName, iconMap) {
+    for (const theme of THEME_NAMES) {
+        const svgFile = path.join(svgDir, theme, `${fileName}.svg`);
+        const svgContent = fs.readFileSync(svgFile, 'utf-8');
+        const $ = cheerio.load(svgContent);
 
-    for (const el of Array.from($('symbol'))) {
-        const $symbol = $(el);
-        const name = $symbol.attr('id').replace(/^(?:svg-)?icon-/, '');
-        const symbolContent = minify($.html($symbol), htmlMinifierOptions);
+        for (const el of Array.from($('symbol'))) {
+            const $symbol = $(el);
+            const name = $symbol.attr('id').replace(/^(?:svg-)?icon-/, '');
+            const symbolContent = minify($.html($symbol), htmlMinifierOptions);
 
-        if (icons.has(name)) {
-            icons.get(name)[theme] = symbolContent;
-        } else {
-            icons.set(name, { [theme]: symbolContent });
+            if (iconMap.has(name)) {
+                iconMap.get(name)[theme] = symbolContent;
+            } else {
+                iconMap.set(name, { [theme]: symbolContent });
+            }
         }
     }
 }
+addIcons('icons', icons);
+addIcons('program-badges', programIcons);
 
 // map DS4 icons that should use the markup of a DS6 icon
 const map4To6 = { 'arrow-down': 'chevron-down-bold' };
-Object.keys(map4To6).forEach(ds4Name => {
+Object.keys(map4To6).forEach((ds4Name) => {
     const ds6Name = map4To6[ds4Name];
     icons.get(ds6Name).ds4 = icons.get(ds4Name).ds4.replace(ds4Name, ds6Name);
 });
 
-for (const [name, themes] of icons) {
-    const iconFolder = path.join(outputDir, `ebay-${name}-icon`);
-    const browserJSON = path.join(iconFolder, 'browser.json');
-    const markoTag = path.join(iconFolder, 'marko-tag.json');
-    const index = path.join(iconFolder, 'index.marko');
+function generateFile(type, iconMap) {
+    for (const [name, themes] of iconMap) {
+        const postfixName = type === 'icon' ? '-icon' : '';
+        const iconFolder = path.join(outputDir, `ebay-${name}${postfixName}`);
+        const browserJSON = path.join(iconFolder, 'browser.json');
+        const markoTag = path.join(iconFolder, 'marko-tag.json');
+        const index = path.join(iconFolder, 'index.marko');
 
-    if (!fs.existsSync(iconFolder)) fs.mkdirSync(iconFolder);
+        if (!fs.existsSync(iconFolder)) fs.mkdirSync(iconFolder);
 
-    for (const theme of THEME_NAMES) {
-        const shortTheme = util.getDSVersion(theme);
-        const filePath = path.join(iconFolder, util.dsFilenames[shortTheme]);
-        const content = themes[theme] ? `module.exports = function() {
+        for (const theme of THEME_NAMES) {
+            const shortTheme = util.getDSVersion(theme);
+            const filePath = path.join(iconFolder, util.dsFilenames[shortTheme]);
+            const content = themes[theme]
+                ? `module.exports = function() {
     // eslint-disable-next-line max-len,quotes
     return ${JSON.stringify(themes[theme])};
-};` : (`module.exports = function() {
+};`
+                : `module.exports = function() {
     if (typeof window !== 'undefined') {
         console.error('${theme} icon not found: ${name}');
     }
     // eslint-disable-next-line max-len,quotes
     return ${missingSVG.replace('{{name}}', name)};
-};`);
+};`;
 
-        fs.writeFileSync(filePath, `${content.trim()}\n`);
+            fs.writeFileSync(filePath, `${content.trim()}\n`);
+        }
+
+        fs.writeFileSync(
+            browserJSON,
+            `${JSON.stringify(
+                {
+                    requireRemap: util.requireRemap,
+                },
+                null,
+                2
+            )}\n`
+        );
+        fs.writeFileSync(markoTag, `${JSON.stringify(markoTagJson, null, 2)}\n`);
+
+        // eslint-disable-next-line max-len
+        fs.writeFileSync(
+            index,
+            `import symbol from "./symbol.js";\nimport symbolDS4 from "./symbol[ds-4].js";\n<ebay-icon ...input _name="${name}" _type="${type}" _themes=[symbol, symbolDS4]/>\n`
+        );
     }
-
-    fs.writeFileSync(browserJSON, `${JSON.stringify({
-        requireRemap: util.requireRemap
-    }, null, 2)}\n`);
-    fs.writeFileSync(markoTag, `${JSON.stringify(markoTagJson, null, 2)}\n`);
-
-    // eslint-disable-next-line max-len
-    fs.writeFileSync(index, `import symbol from "./symbol.js";\nimport symbolDS4 from "./symbol[ds-4].js";\n<ebay-icon ...input _name="${name}" _themes=[symbol, symbolDS4]/>\n`);
 }
+
+generateFile('icon', icons);
+generateFile('program-badge', programIcons);
--- src/components/ebay-icon/test/test.server.js
+++ src/components/ebay-icon/test/test.server.js
@@ -5,14 +5,16 @@ const migrator = require('../migrator');
 const template = require('..');
 
 const iconName = 'mic';
+const progrmaBadgeIcon = 'program-badge-authenticity-guaranteed';
 
 use(require('chai-dom'));
 
 describe('icon', () => {
-    it('renders inline type with title text', async() => {
+    it('renders inline type with title text', async () => {
         const input = {
             _name: iconName,
-            a11yText: 'inline icon'
+            _type: 'icon',
+            a11yText: 'inline icon',
         };
 
         const { getByRole, getByTitle } = await render(template, input);
@@ -23,12 +25,13 @@ describe('icon', () => {
         expect(svg).has.attr('aria-labelledby', title.id);
     });
 
-    it('renders inline type without title text', async() => {
+    it('renders inline type without title text', async () => {
         const input = {
             _name: iconName,
+            _type: 'icon',
             htmlAttributes: {
-                'data-testid': 'icon'
-            }
+                'data-testid': 'icon',
+            },
         };
 
         const { getByTestId } = await render(template, input);
@@ -36,19 +39,69 @@ describe('icon', () => {
         expect(svg).has.attr('aria-hidden', 'true');
     });
 
-    it('renders no-skin-classes', async() => {
+    it('renders no-skin-classes', async () => {
         const input = {
-            _name: iconName, noSkinClasses: true,
+            _name: iconName,
+            noSkinClasses: true,
+            _type: 'icon',
             class: 'custom-class',
             htmlAttributes: {
-                'data-testid': 'icon'
-            }
+                'data-testid': 'icon',
+            },
         };
         const { getByTestId } = await render(template, input);
         const svg = getByTestId('icon');
 
         expect(svg).has.class('custom-class');
         expect(svg).does.not.have.class(`icon--${iconName}`);
+    });
+});
+
+describe('program badges', () => {
+    it('renders inline type with title text', async () => {
+        const input = {
+            _name: progrmaBadgeIcon,
+            _type: 'program-badge',
+            a11yText: 'inline icon',
+        };
+
+        const { getByRole, getByTitle } = await render(template, input);
+        const svg = getByRole('img');
+        const title = getByTitle(input.a11yText);
+        expect(svg).has.class(`program-badge--authenticity-guaranteed`);
+        expect(svg).contains(title);
+        expect(svg).has.attr('aria-labelledby', title.id);
+    });
+
+    it('renders inline type without title text', async () => {
+        const input = {
+            _name: iconName,
+            _type: 'program-badge',
+            htmlAttributes: {
+                'data-testid': 'icon',
+            },
+        };
+
+        const { getByTestId } = await render(template, input);
+        const svg = getByTestId('icon');
+        expect(svg).has.attr('aria-hidden', 'true');
+    });
+
+    it('renders no-skin-classes', async () => {
+        const input = {
+            _name: iconName,
+            noSkinClasses: true,
+            _type: 'program-badge',
+            class: 'custom-class',
+            htmlAttributes: {
+                'data-testid': 'icon',
+            },
+        };
+        const { getByTestId } = await render(template, input);
+        const svg = getByTestId('icon');
+
+        expect(svg).has.class('custom-class');
+        expect(svg).does.not.have.class(`program-badge--authenticity-guaranteed`);
     });
 });
 
