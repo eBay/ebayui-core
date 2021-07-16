3116f9b9905067a793c13637cb4dbbb448a6cb01
Ian McBurnie <ianmcburnie@users.noreply.github.com>
2021-07-15 17:55:07 -0700

    fix(section-notoce): moved aria-roledescription #1461

--- src/components/ebay-section-notice/examples/knobs.js
+++ src/components/ebay-section-notice/examples/knobs.js
@@ -4,6 +4,6 @@ module.exports = () => ({
     status: select('status', [null, 'attention', 'confirmation', 'information'], null, 'Options'),
     icon: select('icon', ['default', 'none'], 'default', 'Options'),
     hasFooter: boolean('has footer?', false, 'Options'),
-    a11yText: text('a11y-text', 'See this notice', 'Accessibility'),
-    a11yRoleDescription: text('a11y-role-description', '', 'Accessibility'),
+    a11yText: text('a11y-text', 'Attention', 'Accessibility'),
+    a11yRoleDescription: text('a11y-role-description', 'Notice', 'Accessibility'),
 });
