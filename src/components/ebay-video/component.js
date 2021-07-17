35636da8ae8edc712ea8f02dd2c73a1ce37a0cfa
austingardner <gardneraustin@protonmail.com>
2021-07-16 19:16:00 -0700

    feat(video): added option to remove volume slider

--- src/components/ebay-video/component.js
+++ src/components/ebay-video/component.js
@@ -77,7 +77,13 @@ module.exports = {
     },
 
     showControls() {
-        this.ui.configure(videoConfig);
+        const copyConfig = Object.assign({}, videoConfig);
+        if (this.state.volumeSlider === false) {
+            copyConfig.controlPanelElements = videoConfig.controlPanelElements.filter(
+                (item) => item !== 'volume'
+            );
+        }
+        this.ui.configure(copyConfig);
 
         // Clear overflow button to make it look like a report button
         const moreVertButton = this.el.querySelector('.shaka-overflow-menu-button');
@@ -115,10 +121,14 @@ module.exports = {
             this.state.action = input.action;
             this.takeAction();
         }
+        if (input.volumeSlider === false) {
+            this.state.volumeSlider = false;
+        }
     },
 
     onCreate() {
         this.state = {
+            volumeSlider: true,
             action: '',
             showLoading: false,
             isLoaded: true,
