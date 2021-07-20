07b3b08fbab9944cd5544c61007f3ccf34e8950b
austingardner <gardneraustin@protonmail.com>
2021-07-19 23:01:41 -0700

    fix(progress-bar): min is an invalid attribute (#1469)

--- src/components/ebay-progress-bar/component.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    getValue(value, min) {
-        const parsedValue = parseInt(value, 10);
-        if (parsedValue <= min) {
-            return min + 1;
-        }
-        return parsedValue;
-    },
-};
