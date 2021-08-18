ad3c1fcf7823e11d60bbb109f53bc102aaeb805d
Andrew Gliga <agliga@ebay.com>
2021-08-18 16:45:33 -0700

    fix: added global subscribe to handle escape key

--- src/components/ebay-tooltip/component-browser.js
+++
@@ -1,9 +0,0 @@
-const eventUtils = require('../../common/event-utils');
-
-module.exports = {
-    handleKeydown(e) {
-        eventUtils.handleEscapeKeydown(e, () => {
-            this.getComponent('base').collapse();
-        });
-    },
-};
