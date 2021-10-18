0dbec1bbf418e73c29ae14b7167263a577690178
austingardner <gardneraustin@protonmail.com>
2021-10-18 16:33:33 -0700

    chore: ran import svg script

--- src/components/ebay-icon/icons/ebay-tick-icon/symbol.js
+++ src/components/ebay-icon/icons/ebay-tick-icon/symbol.js
@@ -1,4 +1,4 @@
 module.exports = function () {
     // eslint-disable-next-line max-len,quotes
-    return '<symbol viewbox="0 0 14 10" id=icon-tick><path d="M4.99 10a1.008 1.008 0 01-.716-.292L.243 5.684c-.343-.399-.32-.994.052-1.366a1.01 1.01 0 011.369-.052l3.325 3.31L12.336.242a1.01 1.01 0 011.37.052c.371.373.394.967.051 1.366L5.695 9.708c-.188.186-.442.291-.706.292z"></path></symbol>';
+    return '<symbol viewbox="0 0 14 10" id=icon-tick><path d="M5 10a.999.999 0 01-.71-.29l-4-4A1 1 0 011.7 4.3L5 7.59 12.29.3a1 1 0 011.41 1.41l-8 8A.999.999 0 015 10z"></path></symbol>';
 };
