45206d935d248d97aeced86de8ab2799e03dbe79
Andrew Gliga <agliga@ebay.com>
2022-02-02 11:38:04 -0800

    fix(storybook): fixed safari (#1600)

--- .storybook/storybook-code-source/index.js
+++ .storybook/storybook-code-source/index.js
@@ -18,8 +18,10 @@ function argsToString(args, plurarls, indent) {
     for (const key in args) {
         if (Object.prototype.hasOwnProperty.call(args, key) && key !== 'renderBody') {
             const val = args[key];
-            const dashCaseKey = key.replace(/(?<=[a-z])[A-Z]/g, (m) => `-${m.toLowerCase()}`);
-            console.log(key, dashCaseKey);
+            const dashCaseKey = key.replace(
+                /([a-z])([A-Z])/g,
+                (m, m1, m2) => `${m1}-${m2.toLowerCase()}`
+            );
             if (Array.isArray(val)) {
                 for (const item of val) {
                     body += tagToString(dashCaseKey, item, plurarls, indent, true);
