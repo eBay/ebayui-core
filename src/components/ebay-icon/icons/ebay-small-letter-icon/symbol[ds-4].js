b7c639a34643765f7079b24ab5fc48865c72d488
Andrew Gliga <agliga@ebay.com>
2022-02-23 16:36:24 -0800

    chore: reimported icons

--- src/components/ebay-icon/icons/ebay-small-letter-icon/symbol[ds-4].js
+++ src/components/ebay-icon/icons/ebay-small-letter-icon/symbol[ds-4].js
@@ -1,4 +1,4 @@
 module.exports = function () {
     // eslint-disable-next-line max-len,quotes
-    return '<symbol id=icon-small-letter viewbox="0 0 18 14" fill=none><path d="M1 0a1 1 0 00-1 1v12a1 1 0 001 1h16a1 1 0 001-1V1a1 1 0 00-1-1H1zm1 12V2h14v10H2z" fill=#111820></path><path d="M13 5L9 8 5 5" stroke=#111820 stroke-width=2 stroke-linecap=round stroke-linejoin=round></path></symbol>';
+    return '<symbol id=icon-small-letter viewbox="0 0 18 14"><path d="M1 0a1 1 0 00-1 1v12a1 1 0 001 1h16a1 1 0 001-1V1a1 1 0 00-1-1H1zm1 12V2h14v10H2z"></path><path d="M13 5L9 8 5 5" stroke-width=2 stroke-linecap=round stroke-linejoin=round></path></symbol>';
 };
