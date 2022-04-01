d4ae8e51ac77221ad4415cad8f5cf03ff611cefb
Andrew Gliga <agliga@ebay.com>
2022-04-01 15:46:54 -0700

    chore: added nyc back

--- scripts/coverage.js
+++ scripts/coverage.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // strip out template.marko entries per https://github.com/marko-js/marko-cli/issues/70
-const nycOutputPath = `${__dirname}/../.coverage`;
+const nycOutputPath = `${__dirname}/../.nyc_output`;
 fs.readdirSync(nycOutputPath).forEach((file) => {
     const filePath = `${nycOutputPath}/${file}`;
     if (file.endsWith('.json')) {
