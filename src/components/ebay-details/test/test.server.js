63afb964179c2ff4ead35ccf24b11a4144425d9a
Andrew Gliga <agliga@ebay.com>
2022-04-14 12:56:15 -0700

    chore: added common snapshots generator

--- src/components/ebay-details/test/test.server.js
+++ src/components/ebay-details/test/test.server.js
@@ -1,42 +1,35 @@
 import { use } from 'chai';
-import { render, prettyDOM } from '@marko/testing-library';
-import snap from 'mocha-snap';
+import { snapshotHTML } from '../../../common/test-utils/snapshots';
 import { testPassThroughAttributes } from '../../../common/test-utils/server';
 import template from '..';
 import * as mock from './mock';
 
-const snapDOM = (node) => snap(prettyDOM(node), '.html', __dirname);
 use(require('chai-dom'));
 
 describe('details', () => {
     it('renders basic version', async () => {
         const input = mock.Default_Details;
-        const { container } = await render(template, input);
-        await snapDOM(container);
+        await snapshotHTML(template, input, __dirname);
     });
 
     it('renders as div version', async () => {
         const input = Object.assign({}, mock.Default_Details, { as: 'div' });
-        const { container } = await render(template, input);
-        await snapDOM(container);
+        await snapshotHTML(template, input, __dirname);
     });
 
     it('renders in open state', async () => {
         const input = mock.Open_Details;
-        const { container } = await render(template, input);
-        await snapDOM(container);
+        await snapshotHTML(template, input, __dirname);
     });
 
     it('renders small version', async () => {
         const input = Object.assign({}, mock.Default_Details, { size: 'small' });
-        const { container } = await render(template, input);
-        await snapDOM(container);
+        await snapshotHTML(template, input, __dirname);
     });
 
     it('renders center version', async () => {
         const input = Object.assign({}, mock.Default_Details, { alignment: 'center' });
-        const { container } = await render(template, input);
-        await snapDOM(container);
+        await snapshotHTML(template, input, __dirname);
     });
 
     testPassThroughAttributes(template);
--- src/components/ebay-tabs/test/test.server.js
+++ src/components/ebay-tabs/test/test.server.js
@@ -1,25 +1,21 @@
 import { use } from 'chai';
-import { render, prettyDOM } from '@marko/testing-library';
-import snap from 'mocha-snap';
+import { snapshotHTML } from '../../../common/test-utils/snapshots';
 
 import template from '..';
 import * as mock from './mock';
 const { testPassThroughAttributes } = require('../../../common/test-utils/server');
 
-const snapDOM = (node) => snap(prettyDOM(node), '.html', __dirname);
 use(require('chai-dom'));
 
 describe('tabs', () => {
     it('renders basic version with 3 tabs and 3 panels', async () => {
         const input = mock.basic3Headings_3Panels_No_Index;
-        const { container } = await render(template, input);
-        await snapDOM(container);
+        await snapshotHTML(template, input, __dirname);
     });
 
     it('renders basic version with 3 tabs and 3 panels on the second panel', async () => {
         const input = mock.basic3Headings_3Panels_1Index;
-        const { container } = await render(template, input);
-        await snapDOM(container);
+        await snapshotHTML(template, input, __dirname);
     });
 
     testPassThroughAttributes(template);
