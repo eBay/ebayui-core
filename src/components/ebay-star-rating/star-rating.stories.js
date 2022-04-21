edcc471b35f92089b012ee1d0970d5a71ab9009a
Andrew Gliga <agliga@ebay.com>
2022-04-21 11:34:17 -0700

    fix: added dropdown for star rating storybook

--- src/components/ebay-star-rating/star-rating.stories.js
+++ src/components/ebay-star-rating/star-rating.stories.js
@@ -28,12 +28,12 @@ export default {
 
     argTypes: {
         value: {
-            control: { type: 'text' },
+            control: { type: 'select' },
+            options: ['0', '1', '1-5', '2', '2-5', '3', '3-5', '4', '4-5', '5'],
             description:
                 'Only for <ebay-star-rating/>. 1 - 5, depending on how many starts are selected. If 0 or null defaults to no stars selected. Can use 2-5 for 2 and a half stars',
         },
         a11yText: {
-            control: { type: 'text' },
             description: 'The aria label for the outer container.',
         },
     },
