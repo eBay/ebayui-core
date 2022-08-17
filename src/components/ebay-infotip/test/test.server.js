4ec29df3b032325496924bb5f2138f66f383612e
Andrew Gliga <agliga@ebay.com>
2022-08-17 08:50:17 -0700

    feat(infotip): changed to show drawer dialog on modal variant

--- src/components/ebay-infotip/test/test.server.js
+++ src/components/ebay-infotip/test/test.server.js
@@ -1,46 +1,26 @@
-import { expect, use } from 'chai';
-import { render } from '@marko/testing-library';
-import template from '..';
-import * as mock from './mock';
+import { composeStories } from '@storybook/marko/dist/testing';
+import { snapshotHTML } from '../../../common/test-utils/snapshots';
+import * as stories from '../infotip.stories';
 
-use(require('chai-dom'));
+const { Standard, OpenOnRender } = composeStories(stories);
+
+const htmlSnap = snapshotHTML(__dirname);
 
 describe('infotip', () => {
     it('renders default infotip', async () => {
-        const input = mock.WithContent;
-        const { getByLabelText, getByText } = await render(template, input);
-        expect(getByLabelText(input.ariaLabel)).has.class('infotip__host');
-        expect(getByText(input.content.renderBody.text)).has.class('infotip__content');
+        await htmlSnap(Standard);
     });
-
-    it('renders infotip with a header', async () => {
-        const input = mock.WithContentAndHeader;
-        const { getByText } = await render(template, input);
-        expect(getByText(input.heading.renderBody.text)).has.class('infotip__heading');
+    it('renders default infotip open', async () => {
+        await htmlSnap(OpenOnRender);
     });
 
     it('renders default infotip disabled', async () => {
-        const input = mock.Disabled;
-        const { getByLabelText } = await render(template, input);
-        expect(getByLabelText(input.ariaLabel)).has.attr('disabled');
+        await htmlSnap(Standard, { disabled: true });
     });
-
-    // TODO: Does not look like this tag passes through class and style?
-    // testPassThroughAttributes(template);
 });
 
 describe('infotip modal', () => {
-    it('renders modal infotip', async () => {
-        const input = mock.ModalWithContent;
-        const { getByLabelText, getAllByLabelText, getByText } = await render(template, input);
-        expect(getAllByLabelText(input.ariaLabel)[0]).has.class('dialog--mini__host');
-        expect(getByText(input.content.renderBody.text)).has.class('lightbox-dialog__main');
-        expect(getByLabelText(input.a11yCloseButtonText)).has.class('lightbox-dialog__close');
-    });
-
-    it('renders modal infotip without header', async () => {
-        const input = Object.assign({}, mock.WithContentAndHeader, { variant: 'modal' });
-        const { queryByText } = await render(template, input);
-        expect(queryByText(input.heading.renderBody.text)).equals(null);
+    it('renders default', async () => {
+        await htmlSnap(Standard, { variant: 'modal' });
     });
 });
