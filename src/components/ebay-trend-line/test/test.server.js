cc10218f692a6dfcade896dabfc8d2fa007ad06b
Martin Lassen <mlassen1@ebay.com>
2022-11-16 13:26:53 -0800

    refactor: added server test for the ebay-trend-line

---
+++ src/components/ebay-trend-line/test/test.server.js
@@ -0,0 +1,45 @@
+import { expect, use } from 'chai';
+import { render } from '@marko/testing-library';
+import template from '..';
+import { useSerialIds } from 'highcharts';
+
+use(require('chai-dom'));
+
+const mock = {
+    data: [
+        {
+            x: 0,
+            y: 0,
+        },
+        {
+            x: 1,
+            y: 1,
+        },
+    ],
+};
+describe('trend-line', () => {
+    it('renders a blue line', async () => {
+        const { getByRole } = await render(template, mock);
+        console.log('trend line render');
+        const line = getByRole('graphics-symbol');
+        expect(line.classList.contains('ebay-trend-line__path-neutral'));
+    });
+
+    it('renders a red line', async () => {
+        const { getByRole } = await render(template, {
+            ...mock,
+            trend: 'negative',
+        });
+        const line = getByRole('graphics-symbol');
+        expect(line.classList.contains('ebay-trend-line__path-negative'));
+    });
+
+    it('renders a green line', async () => {
+        const { getByRole } = await render(template, {
+            ...mock,
+            trend: 'positive',
+        });
+        const line = getByRole('graphics-symbol');
+        expect(line.classList.contains('ebay-trend-line__path-positive'));
+    });
+});
