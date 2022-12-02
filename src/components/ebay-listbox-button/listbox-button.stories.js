b3af3918ca630135ec4d73231cef048dca448f4c
Taylor Hunt <tayhunt@ebay.com>
2022-12-02 13:33:27 -0500

    feat: add “form” variant to listbox-button

--- src/components/ebay-listbox-button/listbox-button.stories.js
+++ src/components/ebay-listbox-button/listbox-button.stories.js
@@ -95,6 +95,12 @@ export default {
                 category: '@option attributes',
             },
         },
+        variant: {
+            description:
+                'If `form`, changes appearance to match other form-specific dropdown-esque components for visual consistency.',
+            options: ['', 'form'],
+            type: 'select',
+        },
 
         onChange: {
             action: 'on-change',
