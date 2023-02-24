aaf729be396bc9c8c21792440e9cb6c772cd6cfc
Andrew Gliga <agliga4@gmail.com>
2023-02-24 08:02:21 -0800

    feat(icons): reimported new icons from skin (#1857)

---
+++ src/components/ebay-icon/icons/ebay-medium-box-24-icon/symbol.js
@@ -0,0 +1,4 @@
+export function symbol() {
+    // eslint-disable-next-line max-len,quotes
+    return '<symbol width=24 height=24 viewbox="0 0 24 24" id=icon-medium-box-24><path d="m20.796 8.395-2.991-3.989A.994.994 0 0 0 16.984 4H7a.998.998 0 0 0-.809.412L3.208 8.39A.997.997 0 0 0 3 9.01V19a1 1 0 0 0 1 1h16a1 1 0 0 0 1-1V9.01a.999.999 0 0 0-.204-.615ZM19 18h-6v-8h6v8Zm-8 0H5v-8h6v8Zm2-10V6h3.5L18 8h-5Zm-2-2v2H6l1.5-2H11Z"></path></symbol>';
+}
