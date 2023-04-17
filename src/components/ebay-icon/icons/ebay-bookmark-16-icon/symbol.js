04781c256cd1dc52de542666d63baf64a47fe7e2
Luke LaValva <lukelavalva@gmail.com>
2023-04-17 12:07:51 -0700

    feat(icons): import new icons from skin (#1884)

---
+++ src/components/ebay-icon/icons/ebay-bookmark-16-icon/symbol.js
@@ -0,0 +1,4 @@
+export function symbol() {
+    // eslint-disable-next-line max-len,quotes
+    return '<symbol viewbox="0 0 16 16" id=icon-bookmark-16><path fill-rule=evenodd clip-rule=evenodd d="M5.5 3a.5.5 0 0 0-.5.5v8.086l2.293-2.293a1 1 0 0 1 1.414 0L11 11.586V3.5a.5.5 0 0 0-.5-.5h-5ZM3 3.5A2.5 2.5 0 0 1 5.5 1h5A2.5 2.5 0 0 1 13 3.5V14a1 1 0 0 1-1.707.707L8 11.414l-3.293 3.293A1 1 0 0 1 3 14V3.5Z"></path></symbol>';
+}
