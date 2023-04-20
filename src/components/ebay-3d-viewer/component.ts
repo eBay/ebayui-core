1ff8dc7c0ffb0b7ee2e740cf00e21657d89ba4e6
LuLaValva <lukelavalva@gmail.com>
2023-04-20 11:15:57 -0700

    feat(3d-viewer): convert to ts

---
+++ src/components/ebay-3d-viewer/component.ts
@@ -0,0 +1,74 @@
+import { CDNLoader } from '../../common/cdn';
+
+interface Input {
+    cdnUrl?: string;
+    version?: string;
+    class?: string | string[] | Record<string, boolean>;
+    a11yText?: string;
+    src?: string;
+    errorText?: string;
+    a11yStartText?: string;
+    a11yLoadText?: string;
+    // TODO: import types from @google/model-viewer instead of
+    // listing manually from https://modelviewer.dev/docs
+    poster?: string;
+    loading?: 'auto' | 'lazy' | 'eager';
+    reveal?: 'auto' | 'manual';
+    'with-credentials'?: boolean;
+}
+
+interface State {
+    showLoading: boolean;
+    isLoaded: boolean;
+    failed: boolean;
+}
+
+export default class extends Marko.Component<Input, State> {
+    declare cdnLoader: CDNLoader;
+    declare viewer: HTMLElement;
+
+    handleError(err: Error) {
+        this.state.failed = true;
+        this.state.isLoaded = true;
+        this.emit('load-error', err);
+    }
+
+    onCreate() {
+        this.state = {
+            showLoading: false,
+            isLoaded: true,
+            failed: false,
+        };
+
+        this.cdnLoader = new CDNLoader(this as any, {
+            key: 'modelViewer',
+            types: ['module'],
+            files: ['model-viewer.min.js'],
+            setLoading: (value) => {
+                this.state.showLoading = value;
+            },
+            handleError: this.handleError.bind(this),
+            handleSuccess: this.handleSuccess.bind(this),
+        });
+    }
+
+    handleSuccess() {
+        this.state.isLoaded = true;
+        this.state.showLoading = false;
+        this.state.failed = false;
+    }
+
+    onMount() {
+        this.viewer = this.getEl('3d-viewer');
+        this._loadViewer();
+    }
+
+    _loadViewer() {
+        this.state.failed = false;
+        this.state.isLoaded = false;
+
+        this.cdnLoader
+            .setOverrides(this.input.cdnUrl ? [this.input.cdnUrl] : undefined, this.input.version)
+            .mount();
+    }
+}
