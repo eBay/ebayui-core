fcb9f46a55d4a5ec50a60e6305aafaf734dea6ce
LuLaValva <lukelavalva@gmail.com>
2023-04-27 10:09:36 -0700

    feat(fake-link): convert to ts

--- src/components/ebay-fake-link/component.ts
+++ src/components/ebay-fake-link/component.ts
@@ -1,23 +1,28 @@
 import * as eventUtils from '../../common/event-utils';
 
-export default {
-    handleClick(originalEvent) {
+interface Input extends Marko.Input<'button'> {
+    toJSON?: any;
+    variant?: 'inline' | 'standalone';
+}
+
+export default class extends Marko.Component<Input> {
+    handleClick(originalEvent: MouseEvent) {
         if (!this.input.disabled) {
             this.emit('click', { originalEvent });
         }
-    },
+    }
 
-    handleKeydown(originalEvent) {
+    handleKeydown(originalEvent: KeyboardEvent) {
         eventUtils.handleEscapeKeydown(originalEvent, () => {
             if (!this.input.disabled) {
                 this.emit('escape', { originalEvent });
             }
         });
-    },
-    handleFocus(originalEvent) {
+    }
+    handleFocus(originalEvent: FocusEvent) {
         this.emit('focus', { originalEvent });
-    },
-    handleBlur(originalEvent) {
+    }
+    handleBlur(originalEvent: FocusEvent) {
         this.emit('blur', { originalEvent });
-    },
-};
+    }
+}
