391498325c3e384662953b3ccfe3ea6e609c6c12
LuLaValva <lukelavalva@gmail.com>
2023-05-02 19:03:37 -0700

    feat(notice): convert to ts

--- src/components/components/ebay-notice-base/component.ts
+++ src/components/components/ebay-notice-base/component.ts
@@ -1,13 +1,25 @@
-module.exports = {
-    handleDismissClick: forwardEvent('dismiss'),
-    handleDismissKeydown: forwardEvent('dismiss'),
-};
-
-function forwardEvent(eventName: string) {
-    return function (value: unknown, originalEvent: Event) {
-        this.emit(eventName, {
-            originalEvent,
-            value: value,
-        });
+export interface Input extends Omit<Marko.Input<'section'>, 'title' | `on${string}`> {
+    status?: 'confirmation' | 'celebration' | 'attention' | 'information';
+    prefixClass?: string;
+    root?: Marko.Renderable;
+    noA11yLabel?: boolean;
+    a11yRoleDescription?: string;
+    icon?: 'none';
+    iconClass?: string;
+    headerRoot?: Marko.Renderable;
+    a11yText?: string;
+    a11yIconText?: string;
+    mainRoot?: Marko.Renderable;
+    footer?: Marko.Renderable;
+    a11yDismissText?: string;
+    title?: Marko.Input<'title'> & {
+        as?: Marko.Renderable;
     };
+    cta?: Marko.Input<'a'> & {
+        renderBody?: Marko.Renderable;
+    };
+    'on-dismiss': () => void;
+    onDismiss?: this['on-dismiss'];
 }
+
+export default class extends Marko.Component<Input> {}
---
+++ src/components/ebay-page-notice/component.ts
@@ -0,0 +1,26 @@
+import { Input as NoticeBaseInput } from '../components/ebay-notice-base/component';
+
+export interface Input extends Omit<NoticeBaseInput, 'role' | 'prefixClass' | `on${string}`> {
+    dismissed?: boolean;
+    'on-dismiss'?: () => void;
+    onDismiss?: this['on-dismiss'];
+}
+
+interface State {
+    dismissed: boolean;
+}
+
+export default class extends Marko.Component<Input, State> {
+    onCreate() {
+        this.state = { dismissed: false };
+    }
+
+    onInput(input: Input) {
+        this.state = { dismissed: input.dismissed || false };
+    }
+
+    onDismiss() {
+        this.state.dismissed = true;
+        this.emit('dismiss');
+    }
+}
