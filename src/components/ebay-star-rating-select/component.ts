4372e875234e5f81d7ae8335dcb53b3b30aeda02
LuLaValva <lukelavalva@gmail.com>
2023-05-10 11:27:04 -0700

    feat(star-rating-select): convert to ts

---
+++ src/components/ebay-star-rating-select/component.ts
@@ -0,0 +1,50 @@
+export interface StarRatingEvent<T extends Event> {
+    originalEvent: T;
+    value: number;
+}
+
+export interface Input extends Omit<Marko.Input<'div'>, `on${string}`> {
+    value?: string;
+    a11yStarText?: string;
+    a11yText?: string;
+    disabled?: boolean;
+    'on-change'?: (event: StarRatingEvent<PointerEvent>) => void;
+    onChange?: this['on-change'];
+    'on-focus'?: (event: StarRatingEvent<FocusEvent>) => void;
+    onFocus?: this['on-focus'];
+    'on-keydown'?: (event: StarRatingEvent<KeyboardEvent>) => void;
+    onKeydown?: this['on-keydown'];
+}
+
+export interface State {
+    value: number;
+}
+
+export default class extends Marko.Component<Input, State> {
+    onCreate() {
+        this.state = {
+            value: 0,
+        };
+    }
+    onInput(input: Input) {
+        let value = parseInt(input.value ?? '') || 0;
+        if (value > 5) {
+            value = 0;
+        }
+        this.state.value = value;
+    }
+    emitEvent<T extends Event>(
+        name: string,
+        value: number,
+        originalEvent: T,
+        el: HTMLInputElement
+    ) {
+        if (!el.disabled) {
+            this.state.value = value;
+            this.emit(name, {
+                originalEvent,
+                value: value,
+            } satisfies StarRatingEvent<T>);
+        }
+    }
+}
