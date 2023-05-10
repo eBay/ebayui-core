60b835e5a4043d3292edb7a76eaab989fc93e375
LuLaValva <lukelavalva@gmail.com>
2023-05-10 12:23:14 -0700

    feat(tabs): convert to ts

--- src/components/ebay-tabs/component.ts
+++ src/components/ebay-tabs/component.ts
@@ -1,14 +1,30 @@
 import { createLinear } from 'makeup-roving-tabindex';
 import * as eventUtils from '../../common/event-utils';
 
-export default {
+export interface Panel extends Omit<Marko.Input<'div'>, `on${string}`> {}
+
+export interface Tab extends Omit<Marko.Input<'div'>, `on${string}`> {}
+
+export interface Input extends Omit<Marko.Input<'div'>, `on${string}`> {
+    tabs: Marko.AttrTag<Tab>[];
+    panels: Marko.AttrTag<Panel>[];
+    activation?: 'auto' | 'manual';
+    fake?: boolean;
+    selectedIndex?: number | string;
+}
+
+export interface State {
+    selectedIndex: number;
+}
+
+export default class extends Marko.Component<Input, State> {
+    declare _linearRovingTabindex: ReturnType<typeof createLinear>;
+
     /**
      * Handle a11y for heading
      * https://ebay.gitbooks.io/mindpatterns/content/disclosure/tabs.html
-     * @param {KeyboardEvent} event
-     * @param {HTMLDivElement} el
      */
-    handleHeadingKeydown(dataIndex, event) {
+    handleHeadingKeydown(dataIndex: number, event: KeyboardEvent) {
         eventUtils.handleActionKeydown(event, () => {
             event.preventDefault();
             this._setIndex(dataIndex);
@@ -22,58 +38,59 @@ export default {
             const keyCode = event.charCode || event.keyCode;
             const direction = keyCode === 37 || keyCode === 38 ? -1 : 1;
             const selectedIndex = (state.selectedIndex + len + direction) % len;
-            this.getEl(`tabs-${selectedIndex}`).focus();
+            (this.getEl(`tabs-${selectedIndex}`) as HTMLElement)?.focus();
 
             if (!input.activation || input.activation === 'auto') {
                 this._setIndex(selectedIndex);
             }
         });
-    },
+    }
 
     handleHeadingClick(selectedIndex) {
         this._setIndex(selectedIndex);
-    },
+    }
 
     onCreate() {
         this.state = { selectedIndex: 0 };
-    },
+    }
 
-    onInput(input) {
+    onInput(input: Input) {
         const { state } = this;
         input.tabs = input.tabs || [];
         input.panels = input.panels || [];
 
-        if (!isNaN(input.selectedIndex)) {
-            state.selectedIndex = parseInt(input.selectedIndex, 10) % (input.tabs.length || 1);
+        if (!isNaN(input.selectedIndex as number)) {
+            state.selectedIndex =
+                parseInt(input.selectedIndex as string, 10) % (input.tabs.length || 1);
         }
-    },
+    }
 
     onMount() {
         this._setupMakeup();
-    },
+    }
 
     onUpdate() {
         this._setupMakeup();
-    },
+    }
 
     onRender() {
         if (typeof window !== 'undefined') {
             this._cleanupMakeup();
         }
-    },
+    }
 
     onDestroy() {
         this._cleanupMakeup();
-    },
+    }
 
-    _setIndex(selectedIndex) {
+    _setIndex(selectedIndex: number) {
         const { state } = this;
 
         if (selectedIndex !== state.selectedIndex) {
             state.selectedIndex = selectedIndex;
             this.emit('select', { selectedIndex });
         }
-    },
+    }
 
     _setupMakeup() {
         const { input, state } = this;
@@ -84,12 +101,12 @@ export default {
                 wrap: true,
             });
         }
-    },
+    }
 
     _cleanupMakeup() {
-        if (this._rovingTabIndex) {
-            this._rovingTabIndex.destroy();
-            this._rovingTabIndex = undefined;
+        if (this._linearRovingTabindex) {
+            this._linearRovingTabindex.destroy();
+            this._linearRovingTabindex = undefined;
         }
-    },
-};
+    }
+}
