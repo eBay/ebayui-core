163bbd5c990c21160e0cdb7665f5749d85d61ee2
LuLaValva <lukelavalva@gmail.com>
2023-05-10 15:54:23 -0700

    feat(icon-button): convert to ts

---
+++ src/components/ebay-icon-button/component-browser.ts
@@ -0,0 +1,43 @@
+import * as eventUtils from '../../common/event-utils';
+
+export interface Input extends Omit<Marko.Input<'button'>, `on${string}`> {
+    toJSON?: any;
+    badgeNumber?: number;
+    href?: string;
+    transparent?: boolean;
+    size?: 'small' | 'large';
+    partiallyDisabled?: boolean;
+    badgeAriaLabel?: string;
+    'on-click'?: (event: { originalEvent: MouseEvent }) => void;
+    onClick?: this['on-click'];
+    'on-escape'?: (event: { originalEvent: KeyboardEvent }) => void;
+    onEscape?: this['on-escape'];
+    'on-focus'?: (event: { originalEvent: FocusEvent }) => void;
+    onFocus?: this['on-focus'];
+    'on-blur'?: (event: { originalEvent: FocusEvent }) => void;
+    onBlur?: this['on-blur'];
+}
+
+export default class extends Marko.Component<Input> {
+    handleClick(originalEvent: MouseEvent) {
+        if (!this.input.disabled) {
+            this.emit('click', { originalEvent });
+        }
+    }
+
+    handleKeydown(originalEvent: KeyboardEvent) {
+        eventUtils.handleEscapeKeydown(originalEvent, () => {
+            if (!this.input.disabled) {
+                this.emit('escape', { originalEvent });
+            }
+        });
+    }
+
+    handleFocus(originalEvent: FocusEvent) {
+        this.emit('focus', { originalEvent });
+    }
+
+    handleBlur(originalEvent: FocusEvent) {
+        this.emit('blur', { originalEvent });
+    }
+}
