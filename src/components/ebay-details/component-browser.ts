a42415dab917da1e5230de0dc3c7a79546341198
LuLaValva <lukelavalva@gmail.com>
2023-05-12 15:09:44 -0700

    feat(details): second ts pass

--- src/components/ebay-details/component-browser.ts
+++ src/components/ebay-details/component-browser.ts
@@ -1,9 +1,13 @@
-interface Input extends Omit<Marko.Input<'details'>, `on${string}`> {
+export interface Input extends Omit<Marko.Input<'details'>, `on${string}`> {
     toJSON?: any;
     text: string;
     size?: 'regular' | 'small';
     alignment?: 'regular' | 'center';
     as?: keyof Marko.NativeTags;
+    'on-toggle'?: (event: { originalEvent: Event; open: boolean }) => void;
+    onToggle?: this['on-toggle'];
+    'on-click'?: (event: { originalEvent: MouseEvent }) => void;
+    onClick?: this['on-click'];
 }
 
 export default class extends Marko.Component<Input> {
