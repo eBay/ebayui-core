6f04c53a3ab5879cd479db5b53d4cb418b9a29ba
Andrew Gliga <agliga@ebay.com>
2023-05-16 11:24:01 -0700

    feat(segmented-buttons): new component (#1902)

---
+++ src/components/ebay-segmented-buttons/README.md
@@ -0,0 +1,14 @@
+<h1 style="display: flex; justify-content: space-between; align-items: center;">
+    <span>
+        ebay-segmented-buttons
+    </span>
+    <span style="font-weight: normal; font-size: medium; margin-bottom: -15px;">
+        DS v1.1.0
+    </span>
+</h1>
+
+## Examples and Documentation
+
+-   [Storybook](https://ebay.github.io/ebayui-core/?path=/story/buttons-ebay-segmented-buttons)
+-   [Storybook Docs](https://ebay.github.io/ebayui-core/?path=/docs/buttons-ebay-segmented-buttons)
+-   [Code Examples](https://github.com/eBay/ebayui-core/tree/master/src/components/ebay-segmented-buttons/examples)
---
+++ src/components/ebay-segmented-buttons/test/test.browser.js
@@ -0,0 +1,55 @@
+import { expect, use } from "chai";
+import chaiDom from "chai-dom";
+import { render, fireEvent, cleanup } from "@marko/testing-library";
+import { composeStories } from "@storybook/marko/dist/testing";
+import * as stories from "../segmented-buttons.stories"; // import all stories from the stories file
+
+const { Default } = composeStories(stories);
+
+use(chaiDom);
+afterEach(cleanup);
+
+let component;
+
+describe("given button is toggled", () => {
+    beforeEach(async () => {
+        component = await render(Default);
+    });
+
+    describe("when button is clicked", () => {
+        beforeEach(async () => {
+            await fireEvent.click(component.getByText("Q2"));
+        });
+
+        it("then it emits the event with correct data", () => {
+            const data = component.emitted("change");
+            expect(data).has.length(1);
+            expect(data[0][0].index).equals(1);
+            expect(data[0][0].value).equals("quarter2");
+        });
+    });
+
+    describe("when the same button is clicked", () => {
+        beforeEach(async () => {
+            await fireEvent.click(component.getByText("Q1"));
+        });
+
+        it("then it emits the event with correct data", () => {
+            const data = component.emitted("change");
+            expect(data).has.length(0);
+        });
+    });
+    describe("when the button is clicked twice", () => {
+        beforeEach(async () => {
+            await fireEvent.click(component.getByText("Q3"));
+            await fireEvent.click(component.getByText("Q3"));
+        });
+
+        it("then it emits the event with correct data", () => {
+            const data = component.emitted("change");
+            expect(data).has.length(1);
+            expect(data[0][0].index).equals(2);
+            expect(data[0][0].value).equals("quarter3");
+        });
+    });
+});
