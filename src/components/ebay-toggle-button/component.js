23e904d4b2abbc16a845d977db6810fd48af1326
Luke LaValva <lukelavalva@gmail.com>
2023-06-21 09:43:02 -0700

    feat(toggle-button): add toggle button (#1922)

---
+++ src/components/ebay-toggle-button/component.js
@@ -0,0 +1,13 @@
+export default class {
+    onInput(input) {
+        this.state = { pressed: input.pressed };
+    }
+
+    handleClick(ev) {
+        this.state.pressed = !this.state.pressed;
+        this.emit("toggle", {
+            originalEvent: ev,
+            pressed: this.state.pressed,
+        });
+    }
+}
