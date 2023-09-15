30a81d36a5afbab3ce5e95c8b637ce5973dec498
LuLaValva <lukelavalva@gmail.com>
2023-09-15 14:50:10 -0700

    fix(chip): resolve test failure

--- src/components/ebay-chip/test/test.server.js
+++ src/components/ebay-chip/test/test.server.js
@@ -2,13 +2,13 @@ import { use } from "chai";
 import { composeStories } from "@storybook/marko/dist/testing";
 import { snapshotHTML } from "../../../common/test-utils/snapshots";
 import * as stories from "../chip.stories"; // import all stories from the stories file
-const { Standard, WithDelete } = composeStories(stories);
+const { Default, WithDelete } = composeStories(stories);
 const htmlSnap = snapshotHTML(__dirname);
 
 use(require("chai-dom"));
 
 it("renders default chip component", async () => {
-    await htmlSnap(Standard);
+    await htmlSnap(Default);
 });
 
 it("renders with close icon when label is present", async () => {
