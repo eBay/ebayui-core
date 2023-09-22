b20c2f063054846cee262647ae9347d6cdff55be
LuLaValva <lukelavalva@gmail.com>
2023-09-22 07:35:35 -0700

    fix(ts): update icon script

--- scripts/import-svg.js
+++ scripts/import-svg.js
@@ -110,7 +110,7 @@ function generateFile(type, iconMap) {
         fs.writeFileSync(
             index,
             `import { symbol } from "./symbol";
-import type { Input as IconInput } from "../../component"
+import type { Input as IconInput } from "../../component-browser"
 export type Input = Omit<IconInput, \`_\${string}\`>;
 <ebay-icon ...input _name="${name}" _type="${type}" _themes=symbol/>
 `
