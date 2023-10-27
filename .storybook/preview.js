4f3ad6cfda91c3fbdbc03ff53b9a6161f0f8c5fe
Luke LaValva <lukelavalva@gmail.com>
2023-10-27 13:59:43 -0400

    fix(storybook): order in storybook 7 (#2011)
    * fix(storybook): order in storybook 7
    
    * docs(storybook): add order for top-level markdown files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -2,12 +2,26 @@ import "./custom-styles.less";
 import "@ebay/skin/tokens";
 import "@ebay/skin/global";
 import "@ebay/skin/marketsans";
-import order from "./storyOrder";
 
 export const parameters = {
     layout: "centered",
     controls: { expanded: true },
-    // options: {
-    //     storySort: order,
-    // },
+    options: {
+        storySort: {
+            order: [
+                "Welcome",
+                "Getting Started",
+                "Contributing",
+                "buttons",
+                "dialogs",
+                "form input",
+                "graphics & icons",
+                "media",
+                "navigation & disclosure",
+                "notices & tips",
+                "progress",
+                "building blocks",
+            ],
+        },
+    },
 };
