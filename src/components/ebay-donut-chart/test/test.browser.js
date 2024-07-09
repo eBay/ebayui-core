05522174c20da4d1b21163268a9c9685d736a9f0
Mike Hobizal <mhobizal@ebay.com>
2024-07-09 15:28:24 -0700

    feat: update browser tests

--- src/components/ebay-donut-chart/test/test.browser.js
+++ src/components/ebay-donut-chart/test/test.browser.js
@@ -1,41 +1,55 @@
 import { expect, use } from "chai";
 import chaiDom from "chai-dom";
-import { render, cleanup, waitFor } from "@marko/testing-library";
+import {
+    render,
+    cleanup,
+    waitFor,
+    fireEvent,
+    screen,
+} from "@marko/testing-library";
 import { composeStories } from "@storybook/marko";
 import * as stories from "../donut-chart.stories";
 
 const { Standard, FiveValues } = composeStories(stories);
+
 use(chaiDom);
 afterEach(cleanup);
 
-describe("given a donut chart", () => {
-    let component;
+/** @type import("@marko/testing-library").RenderResult */
+let component;
 
+describe("given a donut chart", () => {
     beforeEach(async () => {
-        component = await render(Standard);
+        component = await render(Standard, {});
     });
 
     it("that it renders with three paths", async () => {
-        await waitFor(() =>
-            expect(
-                component.container.querySelectorAll(".highcharts-point"),
-            ).to.have.length(3),
-        );
+        await waitFor(() => {
+            expect(component.getAllByRole("img")).to.have.length(3);
+        });
+    });
+
+    describe("when it is hovered", () => {
+        beforeEach(async () => {
+            const path = await screen.findAllByRole("img");
+            await fireEvent.mouseOver(path[0]);
+        });
+
+        it("then it displays a tooltip", async () => {
+            const tooltip = await screen.findByRole("tooltip");
+            expect(tooltip).to.exist;
+        });
     });
 });
 
 describe("given a donut chart with five values", () => {
-    let component;
-
     beforeEach(async () => {
-        component = await render(FiveValues);
+        component = await render(FiveValues, {});
     });
 
     it("that it renders with five paths", async () => {
-        await waitFor(() =>
-            expect(
-                component.container.querySelectorAll(".highcharts-point"),
-            ).to.have.length(5),
-        );
+        await waitFor(() => {
+            expect(component.getAllByRole("img")).to.have.length(5);
+        });
     });
 });
