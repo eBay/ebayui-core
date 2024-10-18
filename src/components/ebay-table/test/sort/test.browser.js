9ba942f666ca6f95836b355e94ea334fec112940
nmin <nmin@ebay.com>
2024-10-18 11:57:03 -0400

    fix: add aria-pressed to sort button

--- src/components/ebay-table/test/sort/test.browser.js
+++ src/components/ebay-table/test/sort/test.browser.js
@@ -23,28 +23,29 @@ describe("given sortable table with Seller column is sorted in ascending order (
         });
         it("then proper sort event should be emitted", async () => {
             expect(sellerColumn).toMatchInlineSnapshot(`
-            <button
-              type="button"
-            >
-              
-              Seller
-              
-               
-              
-              
-              <svg
-                aria-hidden="true"
-                class="icon icon--12"
-                focusable="false"
+              <button
+                aria-pressed="true"
+                type="button"
               >
-                <use
-                  href="#icon-sort-up-12"
-                />
-              </svg>
-              
-              
-            </button>
-          `);
+                
+                Seller
+                
+                 
+                
+                
+                <svg
+                  aria-hidden="true"
+                  class="icon icon--12"
+                  focusable="false"
+                >
+                  <use
+                    href="#icon-sort-up-12"
+                  />
+                </svg>
+                
+                
+              </button>
+            `);
             expect(component.emitted("sort")[0][0]).toMatchInlineSnapshot(`
               {
                 "sorted": {
@@ -73,6 +74,7 @@ describe("given sortable table with Seller column is sorted in ascending order (
             it("then proper sort event should be emitted", async () => {
                 expect(sellerColumn).toMatchInlineSnapshot(`
                   <button
+                    aria-pressed="false"
                     type="button"
                   >
                     
