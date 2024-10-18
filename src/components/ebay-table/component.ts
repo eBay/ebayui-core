dc1460b151d6894f97c21803789f641fc4d1979b
Bill Min <darkbluemn@gmail.com>
2024-10-17 20:47:51 -0400

    feat(ebay-table): selection mode (#2286)

--- src/components/ebay-table/component.ts
+++ src/components/ebay-table/component.ts
@@ -1,7 +1,8 @@
+import { AttrTriState } from "marko/tags-html";
 import { WithNormalizedProps } from "../../global";
+import { CheckboxEvent } from "../ebay-checkbox/component-browser";
 
-// export type ColumnType = "normal" | "numeric" | "row-header" | "none";
-export type TableDensity = "compact" | "relaxed" | "none";
+type TableColRowName = string | number;
 export interface TableHeader extends Omit<Marko.Input<"th">, `on${string}`> {
     columnType?: string; // Use ColumnType after marko fix the ts error for attr tags
     renderBody: Marko.Body;
@@ -11,13 +12,97 @@ export interface TableCell
     renderBody: Marko.Body;
 }
 export interface TableRow extends Omit<Marko.Input<"tr">, `on${string}`> {
+    name?: TableColRowName;
+    selected?: boolean;
     cell: Marko.RepeatableAttrTag<TableCell> | Marko.AttrTag<TableCell>[];
 }
 export interface TableInput extends Omit<Marko.Input<"div">, `on${string}`> {
     header: Marko.RepeatableAttrTag<TableHeader> | Marko.AttrTag<TableHeader>[];
+    mode?: "none" | "selection";
+    allSelected?: AttrTriState;
     row?: Marko.RepeatableAttrTag<TableRow> | Marko.AttrTag<TableRow>[];
-    density?: TableDensity;
+    density?: "compact" | "relaxed" | "none";
+    "a11y-select-all-text"?: string;
+    "a11y-select-row-text"?: string;
+    "on-select"?: (event: {
+        selected: Record<TableColRowName, boolean>;
+        allSelected?: AttrTriState;
+    }) => void;
 }
 export interface Input extends WithNormalizedProps<TableInput> {}
 
-export default class extends Marko.Component<Input> {}
+interface State {
+    selected: Record<TableColRowName, boolean>;
+    allSelected: AttrTriState;
+}
+
+export default class EbayTable extends Marko.Component<Input, State> {
+    onCreate() {
+        this.state = {
+            selected: {},
+            allSelected: "false",
+        };
+    }
+
+    onInput(input: Input) {
+        this.state.selected = this.getSelectedRowStateFromInput(input);
+        this.state.allSelected = this.getAllSelectedState(input);
+    }
+
+    getSelectedRowStateFromInput(input: Input) {
+        const selected: Record<TableColRowName, boolean> = {};
+        if (input.row) {
+            for (const [i, row] of Object.entries([...input.row])) {
+                const name = row.name || i;
+                selected[name] = row.selected || false;
+            }
+        }
+        return selected;
+    }
+
+    getAllSelectedState(input: Input): AttrTriState {
+        if (input.allSelected) {
+            return input.allSelected;
+        }
+        let selectedCount = 0;
+        let rowCount = 0;
+        for (const [_name, selected] of Object.entries(this.state.selected)) {
+            if (selected) {
+                selectedCount++;
+            }
+            rowCount++;
+        }
+        if (selectedCount === 0) {
+            return "false";
+        }
+        if (selectedCount === rowCount) {
+            return "true";
+        }
+        return "mixed";
+    }
+
+    headerSelect() {
+        const { allSelected } = this.state;
+        this.state.selected = [...(this.input.row || [])].reduce(
+            (acc, { name }, i) => {
+                acc[name || i] = allSelected !== "true";
+                return acc;
+            },
+            {} as Record<TableColRowName, boolean>,
+        );
+        this.state.allSelected = allSelected !== "true" ? "true" : "false";
+        this.emit("select", {
+            selected: this.state.selected,
+            allSelected: this.state.allSelected,
+        });
+    }
+
+    rowSelect(name: TableColRowName, { checked }: CheckboxEvent) {
+        this.state.selected[name] = checked;
+        this.setStateDirty("selected");
+        this.state.allSelected = this.getAllSelectedState(this.input);
+        this.emit("select", {
+            selected: this.state.selected,
+        });
+    }
+}
--- src/components/ebay-table/test/test.server.js
+++ src/components/ebay-table/test/test.server.js
@@ -1,23 +1,13 @@
-import { it } from "vitest";
-
 import { composeStories } from "@storybook/marko";
 import { snapshotHTML } from "../../../common/test-utils/snapshots";
 import * as stories from "../table.stories"; // import all stories from the stories file
 
-const { Default, Dense, Relaxed } = composeStories(stories);
-
 const htmlSnap = snapshotHTML(__dirname);
 
 describe("ebay-table", () => {
-    it("renders default", async () => {
-        await htmlSnap(Default);
-    });
-
-    it("renders dense density", async () => {
-        await htmlSnap(Dense);
-    });
-
-    it("renders relaxed density", async () => {
-        await htmlSnap(Relaxed);
-    });
+    for (const [name, story] of Object.entries(composeStories(stories))) {
+        it(`renders ${name}`, async () => {
+            await htmlSnap(story);
+        });
+    }
 });
