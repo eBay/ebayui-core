5ecf691a3894501500bc5de4171299fd76682ba2
Andrew Gliga <agliga4@gmail.com>
2024-10-18 11:26:17 -0700

    chore: review comments

--- src/components/ebay-list/component-browser.ts
+++ src/components/ebay-list/component-browser.ts
@@ -1,12 +1,12 @@
 import type { WithNormalizedProps } from "../../global";
-export interface Item extends Omit<Marko.Input<"item">, `on${string}`> {
-    leading?: Marko.RepeatableAttrTag<any>
-    trailing?: Marko.RepeatableAttrTag<any>
-    "on-click"?: (event: {index: number}) => void;
+export interface ListItem extends Omit<Marko.Input<"item">, `on${string}`> {
+    leading?: Marko.AttrTag<Marko.Input<"div">>;
+    trailing?: Marko.AttrTag<Marko.Input<"div">>;
 }
 
 interface ListInput extends Omit<Marko.Input<"div">, `on${string}`> {
-    items?: Marko.RepeatableAttrTag<Item>;
+    "on-item-click"?: (event: {index: number}) => void;
+    items?: Marko.RepeatableAttrTag<ListItem>;
     separator?: boolean;
 }
 
