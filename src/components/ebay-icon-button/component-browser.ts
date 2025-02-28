a4a32aafd924533d727477cc1b7c97ac5abbce5a
Andrew Gliga <agliga4@gmail.com>
2025-02-28 10:45:15 -0800

    chore: added extends from anchor as well

--- src/components/ebay-icon-button/component-browser.ts
+++ src/components/ebay-icon-button/component-browser.ts
@@ -3,10 +3,9 @@ import * as eventUtils from "../../common/event-utils";
 import type { WithNormalizedProps } from "../../global";
 import type { Input as ButtonInput } from "../ebay-button/index.marko";
 
-interface IconButtonInput extends Omit<Marko.HTML.Button, `on${string}`> {
+interface IconButtonInput extends Omit<Marko.HTML.Button, `on${string}` | "type">, Omit<Marko.HTML.A, `on${string}`> {
     "badge-number"?: number | string;
     href?: string;
-    target?: string;
     transparent?: boolean;
     priority?: "primary" | "secondary" | "tertiary" | "none";
     size?: "small" | "large";
