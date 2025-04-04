76048d6c927e3ba72b1894ab8a3d74ffbb88eafe
Andrew Gliga <agliga4@gmail.com>
2025-04-04 09:56:36 -0700

    chore: updated review comments

--- src/components/ebay-item-tile-group/item-tile-group.stories.ts
+++ src/components/ebay-item-tile-group/item-tile-group.stories.ts
@@ -27,18 +27,6 @@ export default {
             description:
                 "Item to be rendered inside the group. Arguments used are the same as for item-tile. See item-tile for more information.",
         },
-        columns: {
-            control: { type: "object" },
-            description:
-                "Number of columns per screen size. Object with keys: min, xs, sm, md, lg, xl, xl2, xl3, xl4",
-
-            table: {
-                defaultValue: {
-                    summary:
-                        "{ min: 1, xs: 2, sm: 3, md: 4, lg: 6, xl: 8, xl2: 10, xl3: 12, xl4: 14 }",
-                },
-            },
-        },
         layout: {
             control: { type: "select" },
             options: ["gallery", "list"],
